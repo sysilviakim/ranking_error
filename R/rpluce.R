e4422ae71021957c817e4f3bfe76afa82dd235dc
Mona Lisa <yukia887@gmail.com>
2022-10-23 13:47:24 -0400

    Add simulation code

---
+++ R/rpluce.R
@@ -0,0 +1,82 @@
+#############################################################
+# WRITE A FUNCTION TO DRAW SAMPLES FROM PLACKET-LUCE MODLE
+#############################################################
+
+#' @title rpluce
+#'
+#' @description  \code{rpluce} draws samples from Placket-Luce model
+#' @param N total number of samples to draw
+#' @param t total number of items (currently t<27)
+#' @param prob a vector of choice probabilities
+#' 
+#' @return A data frame consisting of drawn samples
+#' @examples
+#' SimRank <- rpluce(N=5000, t=4, prob=c(0.5,0.3,0.2,0.1))
+#' head(SimRank)
+#' @export
+
+# THE FOLLOWING ALGORITHMS FOLLOW Xia (2019, 20-22) "Learning and Decision-Making from Rank Data"
+# ALGORITHM 2.1: Efficient Sampling from Plackett-Luce
+# ALGORITHM 2.2: Efficient Sampling from RUM (Random Utility Model)
+
+##########################################################################
+# ALGORITHM 2.1: Efficient Sampling from Plackett-Luce
+# This is based on the "gamma" parameterization of the PL model
+##########################################################################
+rpluce <- function(N, t, prob){
+
+gamma.true <- prob
+R.set <- matrix(nrow=N, ncol=t, NA) # STORAGE OF RANKINGS with N-rows and t-columns
+item <- letters[1:t]                # VECTOR OF ITEMS (currently, t<27)
+
+# LOOP OVER N ACCESSORS
+for(j in 1:N){
+
+# INITIALIZATION +++++++++++++++++++++++++++++++++++#
+R <- character()      # STORAGE OF RANKING
+A <- item             # INITIAL CHOICE SET
+gamma <- gamma.true   # INITIAL CHOICE PROBABILITY
+M <- length(item) - 1 # NUMBER OF LOOPS
+#+++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# LOOP WITHIN ONE ACCESSOR
+for(i in 1:M){
+
+draw <- rmultinom(n=1, size=1, prob=gamma) # DRAW FROM A MULTINOMIAL PMF
+draw <- as.vector(t(draw))
+
+choice <- A[draw==1]      # ITEM THAT WAS DRAWN (draw==1)
+R <- cbind(R, choice)     # CUMULATIVE ITEMS THAT WERE DRAWN SO FAR
+
+# RE-NORMALIZE THE REMAINING CHOICE SET
+A <- A[draw==0]           # REMOVING T-th CHOICE (DRAWN ABOVE)
+gamma <- gamma[draw==0]   # REMOVING T-th CHOICE's PROBABILITY
+gamma <- gamma/sum(gamma) # NORMALIZE THE CHOICE PROBABILITY
+if(is.na(gamma)){         # This is for when the remaining prob is all 0
+gamma <- c(1, rep(0, (length(gamma)-1)) )  
+}else{}
+
+}
+
+R <- cbind(R, A)
+#colnames(R) <- c("choice1", "choice2", "choice3", "choice4")
+
+R.set[j,] <- R
+
+}
+
+R.set <- as.data.frame(R.set)
+return(R.set) # RETURNING RANKINGS OF t ITEMS FOR N ASSESSORS
+}
+
+
+##########################################################################
+# ALGORITHM 2.2: Efficient Sampling from RUM (Random Utility Model)
+# This is based on the "theta" parameterization of the PL model
+##########################################################################
+
+
+
+
+
+
---
+++ R/sim.R
@@ -0,0 +1,157 @@
+##################################################################################
+# sim.R
+# Created: 10/22/2022
+# Aim: to simulate the effects of pattern ranking with various true distributions
+##################################################################################
+
+library(combinat) # For permn
+library(gtools) # For permute
+library(tidyverse)
+library(styler)
+library(PLMIX) # For switch_ord_rank()
+rm(list=ls())
+
+source(here::here("R/rpluce.R")) # READING THE FUNCTION THAT DRAW FROM PRACKETT-LUCE
+##################################################################
+# Simulation Parameters
+##################################################################
+
+N = 2000 # Number of units
+J = 3  # Number of items
+
+
+##################################################################
+# (1) Generate the Sample Space for Simulation 
+##################################################################
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# Generate the population ranking distribution
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+set.seed(102)
+prob_vec <- rep(1/J, J) # Uniform first-choice probability (can be MODIFIED)
+prob_vec <- c(0.8,0.1,0.1) # A dominates B and C in the first-choice probabiltiy
+
+print(prob_vec)
+
+true_pref <- rpluce(N=N, t=J, prob=prob_vec) %>%
+             as_tibble() 
+
+head(true_pref) # Check
+
+# Check the uniformity
+ncol <- dim(true_pref)[2]
+check <- true_pref %>% unite(order,sep="") 
+table(check) 
+
+# Getting the true ranking for each unit
+true_rank <- PLMIX::rank_ord_switch(data=true_pref, 
+                                   format_input="ordering") %>%
+            as_tibble()
+
+true_rank # Item_1 = A, Item _2 = B, Item_3 = C
+# Note: if (3,2,1) this means A corresponds 3, B 2, and C 1. We use this later.
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# Generate the ordered choice set in survey question
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+randomize <- rep(1/J,J) # Assumption 1: Order randomization
+choice <- rpluce(N=N, t=J, prob=randomize) %>%
+          as_tibble() 
+head(choice)
+
+# Check the uniformity 
+check2 <- choice %>% unite(order,sep="")
+table(check2) 
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# Generate respondents' stated ranked preferences
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# (1) 100% attentive respondents
+
+# Storage for observed patterns
+obs_pattern <- data.frame(pattern1 = integer(),
+                          pattern2 = integer(),
+                          pattern3 = integer())
+
+#----------------------------------------------------------------#
+for(i in 1:dim(true_rank)[1]){ # For each i-th unit in true_rank
+
+# Cast numbers to alphabets  
+vec_pref <- true_rank[i,] %>% reshape2::melt() %>%
+            mutate(variable = case_when(variable=="Item_1" ~ "a",
+                                        variable=="Item_2" ~ "b",
+                                        variable=="Item_3" ~ "c"))
+vec_pref # Check
+
+# Alphabet unit i sees in each position
+position1 <- choice[i,1] %>% pull()
+position2 <- choice[i,2] %>% pull()
+position3 <- choice[i,3] %>% pull()
+
+# Assign a value (rank) for each position given the alphabet unit i sees there
+pattern1 <- vec_pref$value[vec_pref$variable==position1]
+pattern2 <- vec_pref$value[vec_pref$variable==position2]
+pattern3 <- vec_pref$value[vec_pref$variable==position3]
+
+# Combine the result
+comb <- data.frame(pattern1 = pattern1,
+                   pattern2 = pattern2,
+                   pattern3 = pattern3) 
+
+# Stack in the storage
+obs_pattern <- rbind(obs_pattern,comb) 
+
+} # End of i loop
+#----------------------------------------------------------------#
+
+obs_pattern <- obs_pattern %>% unite(obs_rank, sep="")
+table(obs_pattern)
+
+
+# (2) 0% attentive respondents
+# Note: These units only rank according to patterns, not based on preference
+# Assuming uniform random patterns
+perm <- combinat::permn(x=1:J)  # Get all elements in the sample space
+perm <- as.data.frame(do.call(cbind, perm)) %>% 
+        t() %>% 
+        as_tibble() %>% 
+        unite(rank, sep="") %>%
+        pull() %>%
+        sort()
+perm
+
+n_perm <- length(perm)           # Size of the permutation space
+prob_pt <- rep(1/n_perm, n_perm) # Uniform patterns (can be MODIFIED later)
+prob_pt <- c(0.05,0.3,0.15,0.15,0.3,0.05) # Zig-zag orientation
+
+obs_random <- sample(x=perm,size=N, replace=T, prob=prob_pt)
+obs_random
+
+table(obs_random)
+
+##################################################################
+# (2) Compare the observed patterns
+##################################################################
+
+pdf("ObsRanking.pdf", width=8, height=6)
+par(mfrow=c(2,1), mar=c(2.5,2.5,3,2), oma=c(0,0,2,0))
+barplot(table(obs_pattern)/N, 
+        main="100% Attentive (True pref: A >>> B = C)", 
+        col="deepskyblue3", border=F)
+barplot(table(obs_random)/N, 
+        main="0% Attentive (Zig-Zag Orientation)", 
+        col="deepskyblue3", border=F)
+title("*Observed* Ranking Patterns (with Item Order Randomization)",outer=T)
+dev.off()
+
+##################################################################
+# (2) Estimate the Quantities of Interest
+##################################################################
+
+
+
+
+
