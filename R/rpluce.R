1e3ed945b87f3855e2b67ab2a57d2f951adb5ff1
Seo-young Silvia Kim <sskim.research@gmail.com>
2022-10-24 19:53:13 -0400

    styler + no rm(list=ls())

--- R/rpluce.R
+++ R/rpluce.R
@@ -8,14 +8,15 @@
 #' @param N total number of samples to draw
 #' @param t total number of items (currently t<27)
 #' @param prob a vector of choice probabilities
-#' 
+#'
 #' @return A data frame consisting of drawn samples
 #' @examples
-#' SimRank <- rpluce(N=5000, t=4, prob=c(0.5,0.3,0.2,0.1))
+#' SimRank <- rpluce(N = 5000, t = 4, prob = c(0.5, 0.3, 0.2, 0.1))
 #' head(SimRank)
 #' @export
 
-# THE FOLLOWING ALGORITHMS FOLLOW Xia (2019, 20-22) "Learning and Decision-Making from Rank Data"
+# THE FOLLOWING ALGORITHMS FOLLOW Xia (2019, 20-22)
+# --- "Learning and Decision-Making from Rank Data"
 # ALGORITHM 2.1: Efficient Sampling from Plackett-Luce
 # ALGORITHM 2.2: Efficient Sampling from RUM (Random Utility Model)
 
@@ -23,60 +24,51 @@
 # ALGORITHM 2.1: Efficient Sampling from Plackett-Luce
 # This is based on the "gamma" parameterization of the PL model
 ##########################################################################
-rpluce <- function(N, t, prob){
 
-gamma.true <- prob
-R.set <- matrix(nrow=N, ncol=t, NA) # STORAGE OF RANKINGS with N-rows and t-columns
-item <- letters[1:t]                # VECTOR OF ITEMS (currently, t<27)
+rpluce <- function(N, t, prob) {
+  gamma.true <- prob
+  # STORAGE OF RANKINGS with N-rows and t-columns
+  R.set <- matrix(nrow = N, ncol = t, NA)
+  item <- letters[1:t] # VECTOR OF ITEMS (currently, t<27)
 
-# LOOP OVER N ACCESSORS
-for(j in 1:N){
+  # LOOP OVER N ACCESSORS
+  for (j in 1:N) {
+    # INITIALIZATION +++++++++++++++++++++++++++++++++++#
+    R <- character() # STORAGE OF RANKING
+    A <- item # INITIAL CHOICE SET
+    gamma <- gamma.true # INITIAL CHOICE PROBABILITY
+    M <- length(item) - 1 # NUMBER OF LOOPS
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++#
 
-# INITIALIZATION +++++++++++++++++++++++++++++++++++#
-R <- character()      # STORAGE OF RANKING
-A <- item             # INITIAL CHOICE SET
-gamma <- gamma.true   # INITIAL CHOICE PROBABILITY
-M <- length(item) - 1 # NUMBER OF LOOPS
-#+++++++++++++++++++++++++++++++++++++++++++++++++++#
+    # LOOP WITHIN ONE ACCESSOR
+    for (i in 1:M) {
+      # DRAW FROM A MULTINOMIAL PMF
+      draw <- rmultinom(n = 1, size = 1, prob = gamma)
+      draw <- as.vector(t(draw))
 
-# LOOP WITHIN ONE ACCESSOR
-for(i in 1:M){
+      choice <- A[draw == 1] # ITEM THAT WAS DRAWN (draw==1)
+      R <- cbind(R, choice) # CUMULATIVE ITEMS THAT WERE DRAWN SO FAR
 
-draw <- rmultinom(n=1, size=1, prob=gamma) # DRAW FROM A MULTINOMIAL PMF
-draw <- as.vector(t(draw))
+      # RE-NORMALIZE THE REMAINING CHOICE SET
+      A <- A[draw == 0] # REMOVING T-th CHOICE (DRAWN ABOVE)
+      gamma <- gamma[draw == 0] # REMOVING T-th CHOICE's PROBABILITY
+      gamma <- gamma / sum(gamma) # NORMALIZE THE CHOICE PROBABILITY
+      if (is.na(gamma)) { # This is for when the remaining prob is all 0
+        gamma <- c(1, rep(0, (length(gamma) - 1)))
+      } else {}
+    }
 
-choice <- A[draw==1]      # ITEM THAT WAS DRAWN (draw==1)
-R <- cbind(R, choice)     # CUMULATIVE ITEMS THAT WERE DRAWN SO FAR
+    R <- cbind(R, A)
+    # colnames(R) <- c("choice1", "choice2", "choice3", "choice4")
 
-# RE-NORMALIZE THE REMAINING CHOICE SET
-A <- A[draw==0]           # REMOVING T-th CHOICE (DRAWN ABOVE)
-gamma <- gamma[draw==0]   # REMOVING T-th CHOICE's PROBABILITY
-gamma <- gamma/sum(gamma) # NORMALIZE THE CHOICE PROBABILITY
-if(is.na(gamma)){         # This is for when the remaining prob is all 0
-gamma <- c(1, rep(0, (length(gamma)-1)) )  
-}else{}
+    R.set[j, ] <- R
+  }
 
+  R.set <- as.data.frame(R.set)
+  return(R.set) # RETURNING RANKINGS OF t ITEMS FOR N ASSESSORS
 }
-
-R <- cbind(R, A)
-#colnames(R) <- c("choice1", "choice2", "choice3", "choice4")
-
-R.set[j,] <- R
-
-}
-
-R.set <- as.data.frame(R.set)
-return(R.set) # RETURNING RANKINGS OF t ITEMS FOR N ASSESSORS
-}
-
 
 ##########################################################################
 # ALGORITHM 2.2: Efficient Sampling from RUM (Random Utility Model)
 # This is based on the "theta" parameterization of the PL model
 ##########################################################################
-
-
-
-
-
-
--- R/sim.R
+++ R/sim.R
@@ -1,70 +1,72 @@
-##################################################################################
+################################################################################
 # sim.R
 # Created: 10/22/2022
 # Aim: to simulate the effects of pattern ranking with various true distributions
-##################################################################################
+################################################################################
 
 library(combinat) # For permn
 library(gtools) # For permute
 library(tidyverse)
 library(styler)
 library(PLMIX) # For switch_ord_rank()
-rm(list=ls())
 
-source(here::here("R/rpluce.R")) # READING THE FUNCTION THAT DRAW FROM PRACKETT-LUCE
+# READING THE FUNCTION THAT DRAW FROM PRACKETT-LUCE
+source(here::here("R/rpluce.R"))
 ##################################################################
 # Simulation Parameters
 ##################################################################
 
-N = 2000 # Number of units
-J = 3  # Number of items
+N <- 2000 # Number of units
+J <- 3 # Number of items
 
 
 ##################################################################
-# (1) Generate the Sample Space for Simulation 
+# (1) Generate the Sample Space for Simulation
 ##################################################################
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 # Generate the population ranking distribution
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 set.seed(102)
-prob_vec <- rep(1/J, J) # Uniform first-choice probability (can be MODIFIED)
-prob_vec <- c(0.8,0.1,0.1) # A dominates B and C in the first-choice probabiltiy
+prob_vec <- rep(1 / J, J) # Uniform first-choice probability (can be MODIFIED)
+prob_vec <- c(0.8, 0.1, 0.1) # A dominates B and C in the first-choice prob.
 
 print(prob_vec)
 
-true_pref <- rpluce(N=N, t=J, prob=prob_vec) %>%
-             as_tibble() 
+true_pref <- rpluce(N = N, t = J, prob = prob_vec) %>%
+  as_tibble()
 
 head(true_pref) # Check
 
 # Check the uniformity
 
 # Getting the true ranking for each unit
-true_rank <- PLMIX::rank_ord_switch(data=true_pref, 
-                                   format_input="ordering") %>%
-            as_tibble()
+true_rank <- PLMIX::rank_ord_switch(
+  data = true_pref,
+  format_input = "ordering"
+) %>%
+  as_tibble()
 
 true_rank # Item_1 = A, Item _2 = B, Item_3 = C
 # Note: if (3,2,1) this means A corresponds 3, B 2, and C 1. We use this later.
 
 
-check <- true_rank %>% unite(order,sep="") 
-table(check) 
+check <- true_rank %>% unite(order, sep = "")
+table(check)
 
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 # Generate the ordered choice set in survey question
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 
-randomize <- rep(1/J,J) # Assumption 1: Order randomization
-choice <- rpluce(N=N, t=J, prob=randomize) %>%
-          as_tibble() 
+randomize <- rep(1 / J, J) # Assumption 1: Order randomization
+choice <- rpluce(N = N, t = J, prob = randomize) %>%
+  as_tibble()
 head(choice)
 
-# Check the uniformity 
-check2 <- choice %>% unite(order,sep="")
-table(check2) 
+# Check the uniformity
+check2 <- choice %>% unite(order, sep = "")
+table(check2)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 # Generate respondents' stated ranked preferences
@@ -73,118 +75,133 @@ table(check2)
 # (1) 100% attentive respondents
 
 # Storage for observed patterns
-obs_pattern <- data.frame(pattern1 = integer(),
-                          pattern2 = integer(),
-                          pattern3 = integer())
+obs_pattern <- data.frame(
+  pattern1 = integer(),
+  pattern2 = integer(),
+  pattern3 = integer()
+)
 
 #----------------------------------------------------------------#
-for(i in 1:dim(true_rank)[1]){ # For each i-th unit in true_rank
+for (i in 1:dim(true_rank)[1]) { # For each i-th unit in true_rank
 
-# Cast numbers to alphabets  
-vec_pref <- true_rank[i,] %>% reshape2::melt() %>%
-            mutate(variable = case_when(variable=="Item_1" ~ "a",
-                                        variable=="Item_2" ~ "b",
-                                        variable=="Item_3" ~ "c"))
-vec_pref # Check
+  # Cast numbers to alphabets
+  vec_pref <- true_rank[i, ] %>%
+    reshape2::melt() %>%
+    mutate(variable = case_when(
+      variable == "Item_1" ~ "a",
+      variable == "Item_2" ~ "b",
+      variable == "Item_3" ~ "c"
+    ))
+  vec_pref # Check
 
-# Alphabet unit i sees in each position
-position1 <- choice[i,1] %>% pull()
-position2 <- choice[i,2] %>% pull()
-position3 <- choice[i,3] %>% pull()
+  # Alphabet unit i sees in each position
+  position1 <- choice[i, 1] %>% pull()
+  position2 <- choice[i, 2] %>% pull()
+  position3 <- choice[i, 3] %>% pull()
 
-# Assign a value (rank) for each position given the alphabet unit i sees there
-pattern1 <- vec_pref$value[vec_pref$variable==position1]
-pattern2 <- vec_pref$value[vec_pref$variable==position2]
-pattern3 <- vec_pref$value[vec_pref$variable==position3]
+  # Assign a value (rank) for each position given the alphabet unit i sees there
+  pattern1 <- vec_pref$value[vec_pref$variable == position1]
+  pattern2 <- vec_pref$value[vec_pref$variable == position2]
+  pattern3 <- vec_pref$value[vec_pref$variable == position3]
 
-# Combine the result
-comb <- data.frame(pattern1 = pattern1,
-                   pattern2 = pattern2,
-                   pattern3 = pattern3) 
+  # Combine the result
+  comb <- data.frame(
+    pattern1 = pattern1,
+    pattern2 = pattern2,
+    pattern3 = pattern3
+  )
 
-# Stack in the storage
-obs_pattern <- rbind(obs_pattern,comb) 
-
+  # Stack in the storage
+  obs_pattern <- rbind(obs_pattern, comb)
 } # End of i loop
 #----------------------------------------------------------------#
 
-obs_pattern <- obs_pattern %>% unite(obs_rank, sep="")
+obs_pattern <- obs_pattern %>% unite(obs_rank, sep = "")
 table(obs_pattern)
 
 
 # (2) 0% attentive respondents
 # Note: These units only rank according to patterns, not based on preference
 # Assuming uniform random patterns
-perm <- combinat::permn(x=1:J)  # Get all elements in the sample space
-perm <- as.data.frame(do.call(cbind, perm)) %>% 
-        t() %>% 
-        as_tibble() %>% 
-        unite(rank, sep="") %>%
-        pull() %>%
-        sort()
+perm <- combinat::permn(x = 1:J) # Get all elements in the sample space
+perm <- as.data.frame(do.call(cbind, perm)) %>%
+  t() %>%
+  as_tibble() %>%
+  unite(rank, sep = "") %>%
+  pull() %>%
+  sort()
 perm
 
-n_perm <- length(perm)           # Size of the permutation space
-prob_pt <- rep(1/n_perm, n_perm) # Uniform patterns (can be MODIFIED later)
-prob_pt <- c(0.05,0.3,0.15,0.15,0.3,0.05) # Zig-zag orientation
+n_perm <- length(perm) # Size of the permutation space
+prob_pt <- rep(1 / n_perm, n_perm) # Uniform patterns (can be MODIFIED later)
+prob_pt <- c(0.05, 0.3, 0.15, 0.15, 0.3, 0.05) # Zig-zag orientation
 
-obs_random <- sample(x=perm,size=N, replace=T, prob=prob_pt)
-obs_random <- data.frame(obs_rank=obs_random)
+obs_random <- sample(x = perm, size = N, replace = T, prob = prob_pt)
+obs_random <- data.frame(obs_rank = obs_random)
 
 head(obs_random)
 table(obs_random)
 
 
 # (3) 50% attentive respondents (Fixed Order (a,b,c))
-draw1 <- check[1:(N/2),] %>% rename(obs_rank=order)
-draw2 <- tibble(obs_rank = obs_random[1001:N,])
+draw1 <- check[1:(N / 2), ] %>% rename(obs_rank = order)
+draw2 <- tibble(obs_rank = obs_random[1001:N, ])
 
-check_half <- rbind(draw1,draw2) 
+check_half <- rbind(draw1, draw2)
 
 # (3) 50% attentive respondents (item order randomization)
 
-draw3 <-tibble(obs_rank =  obs_pattern[1:(N/2),])
-draw4 <-  tibble(obs_rank = obs_random[1001:N,])
+draw3 <- tibble(obs_rank = obs_pattern[1:(N / 2), ])
+draw4 <- tibble(obs_rank = obs_random[1001:N, ])
 
-obs_half <- rbind(draw3,draw4) 
+obs_half <- rbind(draw3, draw4)
 table(obs_half)
 
 ##################################################################
 # (2) Compare the observed patterns
 ##################################################################
 
-pdf(here::here("fig/ObsRanking.pdf"), width=9, height=6)
-par(mfrow=c(2,3), mar=c(2.5,2.5,3,2), oma=c(0,4,2,0))
-barplot(table(check)/N, main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="A. 100% Attentive")
-mtext("Fixed Order (a,b,c)", 
-      side=2, line=4, cex=1.2, font=2, col=rgb(0.1,0.3,0.5,0.5))
-barplot(table(obs_random)/N, 
-        main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="B. 0% Attentive (Zig-Zag Orientation)")
-barplot(table(check_half)/N, 
-        main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="C. 50% Attentive")
+pdf(here::here("fig/ObsRanking.pdf"), width = 9, height = 6)
+par(mfrow = c(2, 3), mar = c(2.5, 2.5, 3, 2), oma = c(0, 4, 2, 0))
+barplot(table(check) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "A. 100% Attentive")
+mtext("Fixed Order (a,b,c)",
+  side = 2, line = 4, cex = 1.2, font = 2, col = rgb(0.1, 0.3, 0.5, 0.5)
+)
+barplot(table(obs_random) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "B. 0% Attentive (Zig-Zag Orientation)")
+barplot(table(check_half) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "C. 50% Attentive")
 
 
-barplot(table(obs_pattern)/N, 
-        main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="D. 100% Attentive")
-mtext("Item Order Randomization", 
-      side=2, line=4, cex=1.2, font=2, col=rgb(0.1,0.3,0.5,0.5))
-barplot(table(obs_random)/N, 
-        main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="E. 0% Attentive (Zig-Zag Orientation)")
-barplot(table(obs_half)/N, 
-        main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="F. 50% Attentive",)
-title("*Observed* Ranking Patterns",outer=T, col.main="deepskyblue4")
+barplot(table(obs_pattern) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "D. 100% Attentive")
+mtext("Item Order Randomization",
+  side = 2, line = 4, cex = 1.2, font = 2, col = rgb(0.1, 0.3, 0.5, 0.5)
+)
+barplot(table(obs_random) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "E. 0% Attentive (Zig-Zag Orientation)")
+barplot(table(obs_half) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "F. 50% Attentive", )
+title("*Observed* Ranking Patterns", outer = T, col.main = "deepskyblue4")
 
 dev.off()
 
@@ -206,28 +223,40 @@ head(obs_half)
 head(choice)
 
 # 100% Attentive
-obs_pref1 <- choice %>% left_join(obs_pattern) %>%                      # Join two datasets
-             gather("V1", "V2", "V3", key="position", value="item") %>% # Turn into a long format
-             mutate(rank = case_when(position=="V1" ~ str_sub(obs_rank, 1,1),      # First value
-                                     position=="V2" ~ str_sub(obs_rank, 2,2),      # Second value
-                                     position=="V3" ~ str_sub(obs_rank, 3,3))) %>% # Third value
-             arrange(id, rank)
+obs_pref1 <- choice %>%
+  left_join(obs_pattern) %>% # Join two datasets
+  # Turn into a long format
+  gather("V1", "V2", "V3", key = "position", value = "item") %>%
+  mutate(rank = case_when(
+    position == "V1" ~ str_sub(obs_rank, 1, 1), # First value
+    position == "V2" ~ str_sub(obs_rank, 2, 2), # Second value
+    position == "V3" ~ str_sub(obs_rank, 3, 3)
+  )) %>% # Third value
+  arrange(id, rank)
 
 # 0% Attentive
-obs_pref2 <- choice %>% left_join(obs_random) %>%                      # Join two datasets
-             gather("V1", "V2", "V3", key="position", value="item") %>% # Turn into a long format
-             mutate(rank = case_when(position=="V1" ~ str_sub(obs_rank, 1,1),      # First value
-                                     position=="V2" ~ str_sub(obs_rank, 2,2),      # Second value
-                                     position=="V3" ~ str_sub(obs_rank, 3,3))) %>% # Third value
-             arrange(id, rank)
+obs_pref2 <- choice %>%
+  left_join(obs_random) %>% # Join two datasets
+  # Turn into a long format
+  gather("V1", "V2", "V3", key = "position", value = "item") %>%
+  mutate(rank = case_when(
+    position == "V1" ~ str_sub(obs_rank, 1, 1), # First value
+    position == "V2" ~ str_sub(obs_rank, 2, 2), # Second value
+    position == "V3" ~ str_sub(obs_rank, 3, 3)
+  )) %>% # Third value
+  arrange(id, rank)
 
 # 50% Attentive
-obs_pref3 <- choice %>% left_join(obs_half) %>%                      # Join two datasets
-             gather("V1", "V2", "V3", key="position", value="item") %>% # Turn into a long format
-             mutate(rank = case_when(position=="V1" ~ str_sub(obs_rank, 1,1),      # First value
-                                     position=="V2" ~ str_sub(obs_rank, 2,2),      # Second value
-                                     position=="V3" ~ str_sub(obs_rank, 3,3))) %>% # Third value
-             arrange(id, rank)
+obs_pref3 <- choice %>%
+  left_join(obs_half) %>% # Join two datasets
+  # Turn into a long format
+  gather("V1", "V2", "V3", key = "position", value = "item") %>%
+  mutate(rank = case_when(
+    position == "V1" ~ str_sub(obs_rank, 1, 1), # First value
+    position == "V2" ~ str_sub(obs_rank, 2, 2), # Second value
+    position == "V3" ~ str_sub(obs_rank, 3, 3)
+  )) %>% # Third value
+  arrange(id, rank)
 
 # Check the "true" ranking data
 head(obs_pref1)
@@ -237,45 +266,48 @@ head(obs_pref3)
 #----------------------------------------------------------------#
 # 2.1: Distribution of unique rankings (the most cease summary)
 # 100% Attentive Respondents
-freq_pref1 <-  obs_pref1[,c(1,4,5)] %>% 
-               spread(key="rank", value="item") %>%
-               dplyr::select(-id) %>%
-               unite("true_order", sep="") 
+freq_pref1 <- obs_pref1[, c(1, 4, 5)] %>%
+  spread(key = "rank", value = "item") %>%
+  dplyr::select(-id) %>%
+  unite("true_order", sep = "")
 
 # 0% Attentive Respondents
-freq_pref2 <-  obs_pref2[,c(1,4,5)] %>% 
-               spread(key="rank", value="item") %>%
-               dplyr::select(-id) %>%
-               unite("true_order", sep="") 
+freq_pref2 <- obs_pref2[, c(1, 4, 5)] %>%
+  spread(key = "rank", value = "item") %>%
+  dplyr::select(-id) %>%
+  unite("true_order", sep = "")
 
 # 50% Attentive Respondents
-freq_pref3 <-  obs_pref3[,c(1,4,5)] %>% 
-               spread(key="rank", value="item") %>%
-               dplyr::select(-id) %>%
-               unite("true_order", sep="") 
+freq_pref3 <- obs_pref3[, c(1, 4, 5)] %>%
+  spread(key = "rank", value = "item") %>%
+  dplyr::select(-id) %>%
+  unite("true_order", sep = "")
 
 
-# Visualizing What Happens When We Extract "True" Orderings from Data with Item Order Randomization
-pdf(here::here("fig/QOI.pdf"), width=8, height=3)
-par(mfrow=c(1,3), mar=c(2.5,2.5,3,2), oma=c(0,4,2,0))
-barplot(table(freq_pref1)/N, main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="A. 100% Attentive")
-mtext("Item Order Randomization", 
-      side=2, line=4, cex=1.2, font=2, col=rgb(0.1,0.3,0.5,0.5))
-barplot(table(freq_pref2)/N, main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="B. 0% Attentive (Zig-Zag Orientation)")
-barplot(table(freq_pref3)/N, main="", 
-        col="deepskyblue3", border=F)
-title(adj=0, main="C. 50% Attentive")
-title("*Recovered* Preferences",outer=T, col.main="deepskyblue4")
+# Visualizing What Happens When We Extract "True" Orderings from Data
+# with Item Order Randomization
+pdf(here::here("fig/QOI.pdf"), width = 8, height = 3)
+par(mfrow = c(1, 3), mar = c(2.5, 2.5, 3, 2), oma = c(0, 4, 2, 0))
+barplot(table(freq_pref1) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "A. 100% Attentive")
+mtext("Item Order Randomization",
+  side = 2, line = 4, cex = 1.2, font = 2, col = rgb(0.1, 0.3, 0.5, 0.5)
+)
+barplot(table(freq_pref2) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "B. 0% Attentive (Zig-Zag Orientation)")
+barplot(table(freq_pref3) / N,
+  main = "",
+  col = "deepskyblue3", border = F
+)
+title(adj = 0, main = "C. 50% Attentive")
+title("*Recovered* Preferences", outer = T, col.main = "deepskyblue4")
 
 
 dev.off()
 #----------------------------------------------------------------#
-
-
-
-
-
