87d3b4087a47d06f4db378ed630dc0ff00180587
Seo-young Silvia Kim <sskim.research@gmail.com>
2022-10-24 20:20:29 -0400

    add utilities script

---
+++ R/utilities.R
@@ -0,0 +1,11 @@
+# Library and function calls ===================================================
+library(here)
+library(combinat) # For permn
+library(gtools) # For permute
+library(tidyverse)
+library(styler)
+library(PLMIX) # For switch_ord_rank()
+
+# READING THE FUNCTION THAT DRAW FROM PRACKETT-LUCE
+source(here("R", "rpluce.R"))
+
