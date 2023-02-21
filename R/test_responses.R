ba6a67f2229ef370f8261f3d6385974f90f7826d
Seo-young Silvia Kim <sskim.research@gmail.com>
2023-02-21 07:31:49 -0500

    will work on decontaminating tate 2003

--- R/test_responses.R
+++ R/test_responses.R
@@ -1,29 +1,15 @@
 source(here::here("R", "utilities.R"))
-# Qualtrics test responses (Jan 22, 2023)
 
 # Data import ==================================================================
+## Qualtrics test responses
 test_rep <- read_csv(here(
-  "data", "raw", "qualtrics-test-responses",
-  "RankingSalience_January 22, 2023_21.05.csv"
+  "data", "raw", "Non-sincere Ranking_February 21, 2023_03.58.csv"
 )) %>%
   clean_names() %>%
   filter(
     start_date != "Start Date" &
       start_date != '{"ImportId":"startDate","timeZone":"America/Denver"}'
-  )
-
-# Pre-pilot ====================================================================
-## Note that the survey continuously changed with feedback from colleagues
-## Median = 11.2 minutes (good!)
-test_rep %>%
-  filter(status == "IP Address") %>%
-  .$duration_in_seconds %>%
-  {as.numeric(.) / 60} %>%
-  summary()
-
-# Survey test response =========================================================
-## I accidentally went overboard with the number ...
-test_rep <- test_rep %>%
+  ) %>%
   filter(status == "Survey Test") %>%
   ## respondent metadata currently not necessary
   select(
@@ -34,11 +20,12 @@ test_rep <- test_rep %>%
   ) %>%
   ## delete all text variables for now
   select(-contains("_text")) %>%
-  ## I don't understand how Qualtrics text responses are generated
-  filter(!is.na(no_context_3_opts_1_1))
+  ## also delete all timing variables
+  select(-contains("timing"))
 
-# No-context ballot sequences ==================================================
-## 3-option case ---------------------------------------------------------------
+# Chi-square Tests and Power Tests =============================================
+## No-context ballot sequences =================================================
+### 3-option case --------------------------------------------------------------
 temp <- test_rep %>%
   unite(
     "no_context_3opt",
@@ -72,7 +59,7 @@ pwr.chisq.test(w = 0.1, df = (24 - 1), power = 0.8) ## 2,212
 pwr.chisq.test(w = 0.3, df = (24 - 1), power = 0.8) ## 246
 pwr.chisq.test(w = 0.5, df = (24 - 1), power = 0.8) ## 89
 
-## 4-option case ---------------------------------------------------------------
+### 4-option case --------------------------------------------------------------
 temp <- test_rep %>%
   unite(
     "no_context_4opt",
@@ -95,8 +82,8 @@ ES.w1(P0, P1) ## 0.17
 pwr.chisq.test(w = ES.w1(P0, P1), N = 1000, df = (length(temp) - 1)) ## 0.93
 pwr.chisq.test(w = ES.w1(P0, P1), df = (length(temp) - 1), power = 0.8) ## 739
 
-# Letters ======================================================================
-## 3-option case ---------------------------------------------------------------
+## Letters =====================================================================
+### 3-option case --------------------------------------------------------------
 temp <- test_rep %>%
   unite("letters_3opt", sep = "", letters_3_opts_1:letters_3_opts_3) %>%
   .$letters_3opt %>%
@@ -111,7 +98,7 @@ ES.w1(P0, P1) ## 0.04 (tiny!)
 pwr.chisq.test(w = ES.w1(P0, P1), N = 1000, df = (length(temp) - 1)) ## 0.15
 pwr.chisq.test(w = ES.w1(P0, P1), df = (length(temp) - 1), power = 0.8) ## 6,927
 
-## 4-option case ---------------------------------------------------------------
+### 4-option case --------------------------------------------------------------
 temp <- test_rep %>%
   unite("letters_4opt", sep = "", letters_4_opts_1:letters_4_opts_4) %>%
   .$letters_4opt %>%
@@ -126,8 +113,8 @@ ES.w1(P0, P1) ## 0.16
 pwr.chisq.test(w = ES.w1(P0, P1), N = 1000, df = (length(temp) - 1)) ## 0.88
 pwr.chisq.test(w = ES.w1(P0, P1), df = (length(temp) - 1), power = 0.8) ## 842
 
-# Party ID =====================================================================
-## 3-option case ---------------------------------------------------------------
+## Party ID ====================================================================
+### 3-option case --------------------------------------------------------------
 temp <- test_rep %>%
   unite("pid_3cand", sep = "", party_id_3_cands_1:party_id_3_cands_3) %>%
   .$pid_3cand %>%
@@ -142,7 +129,7 @@ ES.w1(P0, P1) ## 0.12
 pwr.chisq.test(w = ES.w1(P0, P1), N = 1000, df = (length(temp) - 1)) ## 0.83
 pwr.chisq.test(w = ES.w1(P0, P1), df = (length(temp) - 1), power = 0.8) ## 928
 
-## 4-option case ---------------------------------------------------------------
+### 4-option case --------------------------------------------------------------
 temp <- test_rep %>%
   unite("pid_4cand", sep = "", party_id_4_cands_1:party_id_4_cands_4) %>%
   .$pid_4cand %>%
@@ -156,3 +143,31 @@ ES.w1(P0, P1) ## 0.13
 
 pwr.chisq.test(w = ES.w1(P0, P1), N = 1000, df = (length(temp) - 1)) ## 0.64
 pwr.chisq.test(w = ES.w1(P0, P1), df = (length(temp) - 1), power = 0.8) ## 1,330
+
+# Item Order Randomization and Anchor Question =================================
+
+## Tate (2003) =================================================================
+## anc_tate1993_do is the variable representing item order randomization
+## in the anchor question
+## Reference choice set is {federal, state, municipal},
+## and the "correct" answer is also {federal, state, municipal}.
+test_rep$anc_tate1993_do <- gsub(
+  "Federal government that create policies that affect people's lives at the federal level",
+  "1",
+  gsub(
+    "State government that create policies that affect people's lives at the state level",
+    "2",
+    gsub(
+      "Municipal government that create policies that affect people's lives at the city level",
+      "3",
+      gsub("\\|", "", test_rep$anc_tate1993_do)
+    )
+  )
+)
+prop.table(table(test_rep$anc_tate1993_do))
+
+test_rep <- test_rep %>%
+  separate(anc_tate1993_do, sep = c(1, 2), into = c("V1", "V2", "V3")) %>%
+  unite(obs, anc_tate1993_1:anc_tate1993_3, sep = "") %>%
+  select(obs, V1, V2, V3, contains("tate"), everything())
+
