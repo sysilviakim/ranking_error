f414f3964399a0485415d359a61b7bc4326028e9
Seo-young Silvia Kim <sskim.research@gmail.com>
2023-02-23 22:14:24 -0500

    simplify code, substitute superseded fxns

--- R/pretest_applications_avg_rank.R
+++ R/pretest_applications_avg_rank.R
@@ -4,7 +4,7 @@ source(here::here("R", "pretest_import.R"))
 tate1993 <- main %>%
   filter(!grepl("9", anc_tate1993) & !grepl("9", app_tate1993))
 
-### Parameters for bootsrapping
+### Parameters for bootstrapping
 N <- nrow(tate1993)
 set.seed(12345)
 indices <- seq(bootstrap_n) %>%
@@ -17,60 +17,54 @@ indices <- seq(bootstrap_n) %>%
   bind_cols()
 assert_that(!identical(sort(indices$x1), sort(indices$x2)))
 
+### Correct answers to anchor question
+correct <- ifelse(tate1993$anc_tate1993 == "123", 1, 0)
+
 ## De-Contaminating ============================================================
-### Naive and de-contaminated estimators of pi ---------------------------------
 ### Naive estimator 1: heroic assumption of zero non-sincere responses
-#### Function to compute average ranks
+est_naive_boot <- seq(bootstrap_n) %>%
+  map(~ avg_rank(tate1993[unlist(indices[, .x]), ], "app_tate1993")) %>%
+  bind_rows()
 
-est_naive_bootstrap <- seq(bootstrap_n) %>%
-  map(~ avg_rank(tate1993$app_tate1993[unlist(indices[, .x])]))
-
-### Convert into a data.frame
-tbl_pi <- do.call(rbind.data.frame, est_naive_bootstrap)
-
-## Unbiased estimation of the proportion of sincere responses (Eq. 9)
-est_p_z1_bootstrap <- seq(bootstrap_n) %>%
+### Unbiased estimation of the proportion of sincere responses (Eq. 9)
+est_p_z1_boot <- seq(bootstrap_n) %>%
   map(
     ~ (mean(correct[unlist(indices[, .x])]) - (1 / factorial(3))) /
       (1 - (1 / factorial(3)))
   )
 
 ### Turn into a vector
-vec_pr_z1 <- do.call(rbind.data.frame, est_p_z1_bootstrap)
-vec_pr_z1 <- pull(vec_pr_z1) # Make it to a vector
+vec_pr_z1 <- unlist(est_p_z1_boot)
 vec_pr_z0 <- 1 - vec_pr_z1
 
 ## Unbiased estimator for the underlying non-sincere dist. in anchor q. (Eq. 10)
-est_pi_anc_naive_bootstrap <- seq(bootstrap_n) %>%
-  map(~ avg_rank(tate1993$anc_tate1993[unlist(indices[, .x])]))
-
-tbl_pi_n_anc <- do.call(rbind.data.frame, est_pi_anc_naive_bootstrap)
+est_anc_naive_boot <- seq(bootstrap_n) %>%
+  map(~ avg_rank(tate1993[unlist(indices[, .x]), ], "anc_tate1993")) %>%
+  bind_rows()
 
 ## De-contaminated ranking data
-est_pi_bootstrap <- (tbl_pi - vec_pr_z0 * tbl_pi_n_anc) / vec_pr_z1
+est_debias_boot <- (est_naive_boot - vec_pr_z0 * est_anc_naive_boot) / vec_pr_z1
 
-gg_est <- est_pi_bootstrap %>%
-  gather("variable", "value") %>%
-  group_by(variable) %>%
-  summarize(
-    mean_val = mean(value),
-    low = quantile(value, probs = 0.025),
-    up = quantile(value, probs = 0.975)
+## Quantiles into a single dataframe
+ggdat <- list(debiased = est_debias_boot, naive = est_naive_boot) %>%
+  imap(
+    ~ .x %>%
+      pivot_longer(everything(), names_to = "variable") %>%
+      group_by(variable) %>%
+      summarize(
+        mean_val = mean(value),
+        low = quantile(value, probs = 0.025),
+        up = quantile(value, probs = 0.975)
+      ) %>%
+      mutate(Estimator = .y)
   ) %>%
-  mutate(Estimator = "De-biased")
-
-gg_raw <- tbl_pi %>%
-  gather("variable", "value") %>%
-  group_by(variable) %>%
-  summarize(
-    mean_val = mean(value),
-    low = quantile(value, probs = 0.025),
-    up = quantile(value, probs = 0.975)
-  ) %>%
-  mutate(Estimator = "Naive")
-
-### Combine data ---------------------------------------------------------------
-ggdat <- rbind(gg_est, gg_raw)
+  bind_rows() %>%
+  mutate(
+    Estimator = case_when(
+      Estimator == "debiased" ~ "De-biased",
+      TRUE ~ "Naive"
+    )
+  )
 
 ### Visualization --------------------------------------------------------------
 p <- ggplot(ggdat, aes(x = variable, y = mean_val, color = Estimator)) +
@@ -91,6 +85,6 @@ p <- ggplot(ggdat, aes(x = variable, y = mean_val, color = Estimator)) +
 
 p
 ggsave(
-  here("fig/application_tate1993_bootstrapped_avg_rank.pdf"),
+  here("fig/application_tate1993_bootped_avg_rank.pdf"),
   width = 4, height = 4
 )
