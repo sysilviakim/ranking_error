bcfedb8c5ac0615c89ef98c7e01d2c939937818a
Seo-young Silvia Kim <sskim.research@gmail.com>
2023-06-07 15:25:53 -0400

    attention checks (do partial rankers later)

--- R/pretest/02_pretest_attention_checks.R
+++ R/pretest/02_pretest_attention_checks.R
@@ -12,15 +12,18 @@ main %>%
 ## Identity: 72.4%
 ## Polarization: 46.0%
 
+## Yikes.
+sort(table(main$anc_e_systems), decreasing = TRUE)
+sort(table(main$anc_identity), decreasing = TRUE)
+
 # Correlation between attention filters ========================================
-## Between two attention filters? ----------------------------------------------
 cor_and_condprob(main, "ternovsky_fail", "berinsky_fail")
-# [1] 0.4462482
+# [1] 0.5763128
 # Cond. on berinsky_fail == 1, Pr(ternovsky_fail == 1) is 63.3%
 # Cond. on ternovsky_fail == 1, Pr(berinsky_fail == 1) is 76.0%
 
 # Correlation between attention filters and geometric ==========================
-### Tate 2003 ------------------------------------------------------------------
+## Tate 1993 -------------------------------------------------------------------
 cor_and_condprob(main, "ternovsky_fail", "ns_tate")
 # [1] 0.2347556
 # Cond. on ns_tate == 1, Pr(ternovsky_fail == 1) is 36.2%
@@ -31,18 +34,18 @@ cor_and_condprob(main, "berinsky_fail", "ns_tate")
 # Cond. on ns_tate == 1, Pr(berinsky_fail == 1) is 53.2%
 # Cond. on berinsky_fail == 1, Pr(ns_tate == 1) is 83.3%
 
-### Electoral systems ----------------------------------------------------------
+## Electoral systems -----------------------------------------------------------
 cor_and_condprob(main, "ternovsky_fail", "ns_esystem")
-# [1] 0.341133
-# Cond. on ns_esystem == 1, Pr(ternovsky_fail == 1) is 25.5%
+# [1] 0.2487798
+# Cond. on ns_esystem == 1, Pr(ternovsky_fail == 1) is 30.1%
 # Cond. on ternovsky_fail == 1, Pr(ns_esystem == 1) is 100.0%
 
 cor_and_condprob(main, "berinsky_fail", "ns_esystem")
-# [1] 0.5389262
-# Cond. on ns_esystem == 1, Pr(berinsky_fail == 1) is 30.6%
-# Cond. on berinsky_fail == 1, Pr(ns_esystem == 1) is 100.0%
+# [1] 0.2208731
+# Cond. on ns_esystem == 1, Pr(berinsky_fail == 1) is 34.9%
+# Cond. on berinsky_fail == 1, Pr(ns_esystem == 1) is 96.7%
 
-### Identity -------------------------------------------------------------------
+## Identity --------------------------------------------------------------------
 cor_and_condprob(main, "ternovsky_fail", "ns_identity")
 # [1] 0.3084845
 # Cond. on ns_identity == 1, Pr(ternovsky_fail == 1) is 33.8%
@@ -50,153 +53,61 @@ cor_and_condprob(main, "ternovsky_fail", "ns_identity")
 
 cor_and_condprob(main, "berinsky_fail", "ns_identity")
 # [1] 0.2609297
-# Cond. on ns_identity == 1, Pr(berinsky_fail == 1) is 58.6%
-# Cond. on berinsky_fail == 1, Pr(ns_identity == 1) is 97.1%
+# Cond. on ns_identity == 1, Pr(berinsky_fail == 1) is 38.0%
+# Cond. on berinsky_fail == 1, Pr(ns_identity == 1) is 90.0%
 
-### Voting mode ----------------------------------------------------------------
-# cor_and_condprob(main, "ternovsky_fail", "ns_voting")
-# cor_and_condprob(main, "berinsky_fail", "ns_voting")
+## Polarization ----------------------------------------------------------------
+cor_and_condprob(main, "ternovsky_fail", "ns_polar")
+# [1] 0.4002311
+# Cond. on ns_polar == 1, Pr(ternovsky_fail == 1) is 44.4%
+# Cond. on ternovsky_fail == 1, Pr(ns_polar == 1) is 80%
 
-## Between nonsincerity checks? ------------------------------------------------
-## Tate and Nelson (relatively) don't align well, but this seems to be a Tate
-## thing. Identity is more predictive, conditionally.
+cor_and_condprob(main, "berinsky_fail", "ns_polar")
+# [1] 0.4098351
+# Cond. on ns_polar == 1, Pr(berinsky_fail == 1) is 51.1%
+# Cond. on berinsky_fail == 1, Pr(ns_polar == 1) is 76.7%
 
-### Tate 2003 x Nelson 1997 ----------------------------------------------------
-cor_and_condprob(main, "ns_tate", "ns_nelson")
-# [1] 0.3194393
-# Cond. on ns_nelson == 1, Pr(ns_tate == 1) is 63.3%
-# Cond. on ns_tate == 1, Pr(ns_nelson == 1) is 66%
+# Between geometric responses? =================================================
+## Tate x ESystems
+cor_and_condprob(main, "ns_tate", "ns_esystem")
+# [1] 0.294645
+# Cond. on ns_esystem == 1, Pr(ns_tate == 1) is 54.2%
+# Cond. on ns_tate == 1, Pr(ns_esystem == 1) is 95.7%
 
-### Tate 2003 x Identity -------------------------------------------------------
+## Tate x Identity
 cor_and_condprob(main, "ns_tate", "ns_identity")
-# [1] 0.4765874
-# Cond. on ns_identity == 1, Pr(ns_tate == 1) is 67.2%
-# Cond. on ns_tate == 1, Pr(ns_identity == 1) is 83%
+# [1] 0.3634062
+# Cond. on ns_identity == 1, Pr(ns_tate == 1) is 59.2%
+# Cond. on ns_tate == 1, Pr(ns_identity == 1) is 89.4%
 
-### Tate 2003 x Voting mode ----------------------------------------------------
-# cor_and_condprob(main, "ns_tate", "ns_voting")
+## Tate x Polarization
+cor_and_condprob(main, "ns_tate", "ns_polar")
+# [1] 0.4680065
+# Cond. on ns_polar == 1, Pr(ns_tate == 1) is 73.3%
+# Cond. on ns_tate == 1, Pr(ns_polar == 1) is 70.2%
 
-### Nelson 1997 x Identity -----------------------------------------------------
-cor_and_condprob(main, "ns_nelson", "ns_identity")
-# [1] 0.5503995
-# Cond. on ns_identity == 1, Pr(ns_nelson == 1) is 72.4%
-# Cond. on ns_nelson == 1, Pr(ns_identity == 1) is 85.7%
+## ESystems x Identity
+cor_and_condprob(main, "ns_esystem", "ns_identity")
+# [1] 0.4356316
+# Cond. on ns_identity == 1, Pr(ns_esystem == 1) is 94.4%
+# Cond. on ns_esystem == 1, Pr(ns_identity == 1) is 80.7%
 
-### Nelson 1997 x Voting mode --------------------------------------------------
-cor_and_condprob(main, "ns_nelson", "ns_voting")
+## ESystems x Polarization
+cor_and_condprob(main, "ns_esystem", "ns_polar")
+# [1] 0.3348474
+# Cond. on ns_polar == 1, Pr(ns_esystem == 1) is 97.8%
+# Cond. on ns_esystem == 1, Pr(ns_polar == 1) is 53%
 
-### Identity x Voting mode -----------------------------------------------------
-cor_and_condprob(main, "ns_identity", "ns_voting")
+## Identity x Polarization
+cor_and_condprob(main, "ns_identity", "ns_polar")
+# [1] 0.4765615
+# Cond. on ns_polar == 1, Pr(ns_identity == 1) is 95.6%
+# Cond. on ns_identity == 1, Pr(ns_polar == 1) is 60.6%
 
 # Partial rankers ==============================================================
 ## Who are the partial rankers?
 ## Can inattention predict partial ranking?
 
-## Attention filter x partial ranking? -----------------------------------------
-### Tate 1993 main question ----------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_tate_main")
-# [1] 0.4665595
-# Cond. on partial_tate_main == 1, Pr(ternovsky_fail == 1) is 85.7%
-# Cond. on ternovsky_fail == 1, Pr(partial_tate_main == 1) is 31.6%
+# Demographic correlations? ====================================================
 
-cor_and_condprob(main, "berinsky_fail", "partial_tate_main")
-# [1] 0.3738783
-# Cond. on partial_tate_main == 1, Pr(berinsky_fail == 1) is 100%
-# Cond. on berinsky_fail == 1, Pr(partial_tate_main == 1) is 20%
-
-### Tate 1993 anchor question --------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_tate_anc")
-# [1] 0.5148993
-# Cond. on partial_tate_anc == 1, Pr(ternovsky_fail == 1) is 87.5%
-# Cond. on ternovsky_fail == 1, Pr(partial_tate_anc == 1) is 36.8%
-
-cor_and_condprob(main, "berinsky_fail", "partial_tate_anc")
-# [1] 0.3245785
-# Cond. on partial_tate_anc == 1, Pr(berinsky_fail == 1) is 87.5%
-# Cond. on berinsky_fail == 1, Pr(partial_tate_anc == 1) is 20%
-
-### Nelson 1997 main question --------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_nelson_main")
-# [1] 0.3188285
-# Cond. on partial_nelson_main == 1, Pr(ternovsky_fail == 1) is 50%
-# Cond. on ternovsky_fail == 1, Pr(partial_nelson_main == 1) is 36.8%
-
-cor_and_condprob(main, "berinsky_fail", "partial_nelson_main")
-# [1] 0.2477307
-# Cond. on partial_nelson_main == 1, Pr(berinsky_fail == 1) is 64.3%
-# Cond. on berinsky_fail == 1, Pr(partial_nelson_main == 1) is 25.7%
-
-### Nelson 1997 anchor question ------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_nelson_anc")
-# [1] 0.3185416
-# Cond. on partial_nelson_anc == 1, Pr(ternovsky_fail == 1) is 54.5%
-# Cond. on ternovsky_fail == 1, Pr(partial_nelson_anc == 1) is 31.6
-
-cor_and_condprob(main, "berinsky_fail", "partial_nelson_anc")
-# [1] 0.2780776
-# Cond. on partial_nelson_anc == 1, Pr(berinsky_fail == 1) is 72.7%
-# Cond. on berinsky_fail == 1, Pr(partial_nelson_anc == 1) is 22.9%
-
-### Identity main question -----------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_identity_main")
-# [1] 0.3135416
-# Cond. on partial_identity_main == 1, Pr(ternovsky_fail == 1) is 42.9%
-# Cond. on ternovsky_fail == 1, Pr(partial_identity_main == 1) is 47.4%
-
-cor_and_condprob(main, "berinsky_fail", "partial_identity_main")
-# [1] 0.3423007
-# Cond. on partial_identity_main == 1, Pr(berinsky_fail == 1) is 66.7%
-# Cond. on berinsky_fail == 1, Pr(partial_identity_main == 1) is 40%
-
-### Identity anchor question ---------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_identity_anc")
-# [1] 0.4594157
-# Cond. on partial_identity_anc == 1, Pr(ternovsky_fail == 1) is 58.8%
-# Cond. on ternovsky_fail == 1, Pr(partial_identity_anc == 1) is 52.6%
-
-cor_and_condprob(main, "berinsky_fail", "partial_identity_anc")
-# [1] 0.3934911
-# Cond. on partial_identity_anc == 1, Pr(berinsky_fail == 1) is 76.5%
-# Cond. on berinsky_fail == 1, Pr(partial_identity_anc == 1) is 37.1%
-
-### Voting mode main question --------------------------------------------------
-cor_and_condprob(main, "ternovsky_fail", "partial_voting_main")
-# [1] 0.2852502
-# Cond. on partial_voting_main == 1, Pr(ternovsky_fail == 1) is 42.1%
-# Cond. on ternovsky_fail == 1, Pr(partial_voting_main == 1) is 42.1%
-
-cor_and_condprob(main, "berinsky_fail", "partial_voting_main")
-# [1] 0.1790337
-# Cond. on partial_voting_main == 1, Pr(berinsky_fail == 1) is 52.6%
-# Cond. on berinsky_fail == 1, Pr(partial_voting_main == 1) is 28.6%
-
-### Voting mode anchor question ------------------------------------------------
-# cor_and_condprob(main, "ternovsky_fail", "partial_voting_anc")
-# cor_and_condprob(main, "berinsky_fail", "partial_voting_anc")
-
-## Between partial rankings? ---------------------------------------------------
-## I don't want to do all length(combn(8, 2)) = 56 combinations, so...
-
-### Within Tate 1993 -----------------------------------------------------------
-cor_and_condprob(main, "partial_tate_main", "partial_tate_anc")
-# Cond. on partial_tate_anc == 1, Pr(partial_tate_main == 1) is 62.5%
-# Cond. on partial_tate_main == 1, Pr(partial_tate_anc == 1) is 71.4%
-
-### Within Nelson 1997 ---------------------------------------------------------
-cor_and_condprob(main, "partial_nelson_main", "partial_nelson_anc")
-# [1] 0.6871226
-# Cond. on partial_nelson_anc == 1, Pr(partial_nelson_main == 1) is 81.8%
-# Cond. on partial_nelson_main == 1, Pr(partial_nelson_anc == 1) is 64.3%
-
-### Within Identity ------------------------------------------------------------
-cor_and_condprob(main, "partial_identity_main", "partial_identity_anc")
-# [1] 0.6817068
-# Cond. on partial_identity_anc == 1, Pr(partial_identity_main == 1) is 82.4%
-# Cond. on partial_identity_main == 1, Pr(partial_identity_anc == 1) is 66.7%
-
-### Within Voting Mode ---------------------------------------------------------
-cor_and_condprob(main, "partial_voting_main", "partial_voting_anc")
-
-## Any demographic correlations? -----------------------------------------------
-
-## Duration of completion? -----------------------------------------------------
+# Duration of completion? ======================================================
