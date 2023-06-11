c907d3f8143013fb11a689611b86b95dab6432e2
Seo-young Silvia Kim <sskim.research@gmail.com>
2023-06-11 15:10:07 -0400

    styler::Styler

--- R/pretest/01_pretest_sanity_checks.R
+++ R/pretest/01_pretest_sanity_checks.R
@@ -60,35 +60,42 @@ col <- c("#b0015a", "#128ba0", "#a5900d", "gray")
 
 ## Visualize and save
 
-pdf(here::here("fig", "pretest03-check_duration.pdf"), width=6, height=6)
-par(mfrow=(c(2,2)))
+pdf(here::here("fig", "pretest03-check_duration.pdf"), width = 6, height = 6)
+par(mfrow = (c(2, 2)))
 
-plot(density(time$time_anc_tate_1993_page_submit), 
-     type="n", main="Representation (J=3)\n48% geometric", 
-     xlab="Completion time in seconds (N=101)", xlim=c(0,200))
-lines(density(time$time_app_tate_1993_page_submit), col=col[1])
-lines(density(time$time_anc_tate_1993_page_submit), col=col[1], lty=2)
-legend(x=100, y=0.018, legend=c("Main", "Anchor"),
-       col=col[1], lty=1:2, cex=1, box.lty=0)
+plot(
+  density(time$time_anc_tate_1993_page_submit),
+  type = "n", main = "Representation (J=3)\n48% geometric",
+  xlab = "Completion time in seconds (N=101)", xlim = c(0, 200)
+)
+lines(density(time$time_app_tate_1993_page_submit), col = col[1])
+lines(density(time$time_anc_tate_1993_page_submit), col = col[1], lty = 2)
+legend(
+  x = 100, y = 0.018, legend = c("Main", "Anchor"),
+  col = col[1], lty = 1:2, cex = 1, box.lty = 0
+)
 
-plot(density(time$timing_app_e_systems_page_submit), 
-     type="n", main="Electoral Systems (J=7)\n84.7% geometric", 
-     xlab="Completion time in seconds (N=101)", xlim=c(0,200))
-lines(density(time$time_anc_e_systems_page_submit), col=col[2])
-lines(density(time$timing_app_e_systems_page_submit), col=col[2], lty=2)
+plot(
+  density(time$timing_app_e_systems_page_submit),
+  type = "n", main = "Electoral Systems (J=7)\n84.7% geometric",
+  xlab = "Completion time in seconds (N=101)", xlim = c(0, 200)
+)
+lines(density(time$time_anc_e_systems_page_submit), col = col[2])
+lines(density(time$timing_app_e_systems_page_submit), col = col[2], lty = 2)
 
-plot(density(time$time_app_identity_page_submit), 
-     type="n", main="Identity (J=7)\n72.4% geometric", 
-     xlab="Completion time in seconds (N=101)", xlim=c(0,200))
-lines(density(time$time_app_identity_page_submit), col=col[3])
-lines(density(time$time_anc_identity_page_submit), col=col[3], lty=2)
+plot(
+  density(time$time_app_identity_page_submit),
+  type = "n", main = "Identity (J=7)\n72.4% geometric",
+  xlab = "Completion time in seconds (N=101)", xlim = c(0, 200)
+)
+lines(density(time$time_app_identity_page_submit), col = col[3])
+lines(density(time$time_anc_identity_page_submit), col = col[3], lty = 2)
 
-plot(density(time$time_app_aff_polar_page_submit), 
-     type="n", main="Polarization (J=8)\n46.0% geometric", 
-     xlab="Completion time in seconds (N=101)", xlim=c(0,200))
-lines(density(time$time_app_aff_polar_page_submit), col=col[4])
-lines(density(time$time_anc_polar_page_submit), col=col[4], lty=2)
+plot(density(time$time_app_aff_polar_page_submit),
+  type = "n", main = "Polarization (J=8)\n46.0% geometric",
+  xlab = "Completion time in seconds (N=101)", xlim = c(0, 200)
+)
+lines(density(time$time_app_aff_polar_page_submit), col = col[4])
+lines(density(time$time_anc_polar_page_submit), col = col[4], lty = 2)
 
 dev.off()
-
-
