6054fb319dd1ecdcda4484bf5f932d89d9ec829f
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2023-06-29 04:21:39 -0400

    emphasis point for each app

--- R/04_applications_avg.R
+++ R/04_applications_avg.R
@@ -26,10 +26,10 @@ prep_list <- root_var %>%
 
       full <- full %>%
         select(-contains("repeat"))
-      
+
       return(
         list(
-          full = full, rational = rational, 
+          full = full, rational = rational,
           labels = names(crosswalk),
           full_N = nrow(full), rational_N = nrow(rational)
         )
@@ -47,25 +47,25 @@ prep_list %>%
   )
 
 # $tate
-# 1st 2nd 3rd 
-# 1.7 2.0 2.2 
-# 
+# 1st 2nd 3rd
+# 1.7 2.0 2.2
+#
 # $identity
-# 1st 2nd 3rd 4th 
-# 3.0 2.6 1.9 2.5 
-# 
+# 1st 2nd 3rd 4th
+# 3.0 2.6 1.9 2.5
+#
 # $polar
-# 1st 2nd 3rd 4th 5th 
-# 2.3 2.6 2.9 3.1 4.2 
-# 
+# 1st 2nd 3rd 4th 5th
+# 2.3 2.6 2.9 3.1 4.2
+#
 # $esystems
-# 1st 2nd 3rd 4th 5th 6th 
+# 1st 2nd 3rd 4th 5th 6th
 # 2.7 2.7 4.1 4.1 4.3 3.1
 
 prep_list %>%
   imap(
     ~ avg_rank(.x$full, paste0("app_", .y)) %>%
-      map(~ .x) %>% 
+      map(~.x) %>%
       bind_rows(.id = "reference") %>%
       rowwise() %>%
       mutate(varname = paste0("app_", .y, "_", substr(reference, 1, 1))) %>%
@@ -76,3 +76,51 @@ prep_list %>%
       select(reference, varname, crosswalk, everything())
   )
 
+# Avg., pair, top-k, and marginal rankings =====================================
+p1 <- vis_ranking(
+  dat = prep_list$tate$full,
+  target_item = "pork",
+  other_items = setdiff(prep_list$tate$labels, "pork")
+)
+
+p2 <- vis_ranking(
+  dat = prep_list$identity$full,
+  target_item = "party",
+  other_items = setdiff(prep_list$identity$labels, "party")
+)
+
+p3 <- vis_ranking(
+  dat = prep_list$polar$full,
+  target_item = "social_media",
+  other_items = setdiff(prep_list$polar$labels, "social_media")
+)
+
+p4 <- vis_ranking(
+  dat = prep_list$esystems$full,
+  target_item = "account_pol",
+  other_items = setdiff(prep_list$esystems$labels, "account_pol")
+)
+
+print(p1)
+ggsave(
+  here("fig", "tate_pork_emphasized.pdf"),
+  width = 6.5, height = 4
+)
+
+print(p2)
+ggsave(
+  here("fig", "identity_party_emphasized.pdf"),
+  width = 6.5, height = 4
+)
+
+print(p3)
+ggsave(
+  here("fig", "polar_social_media_emphasized.pdf"),
+  width = 6.5, height = 4
+)
+
+print(p4)
+ggsave(
+  here("fig", "esystems_account_pol_emphasized.pdf"),
+  width = 6.5, height = 4
+)
