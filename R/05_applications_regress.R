132e7b055771aff6439d53241fcc5687900317c7
Atsusaka <yatsusak@CougarNet.UH.EDU>
2023-10-13 18:55:26 -0500

    visualize rank-order logit

--- R/05_applications_regress.R
+++ R/05_applications_regress.R
@@ -9,6 +9,7 @@ class(main$app_identity_1) <- "numeric"
 class(main$app_identity_2) <- "numeric"
 class(main$app_identity_3) <- "numeric"
 class(main$app_identity_4) <- "numeric"
+class(main$ideo7) <- "numeric"
 
 # Reference set: (party, religion, gender, race)
 
@@ -19,14 +20,15 @@ dt <- main %>%
          ch.religion = app_identity_2,
          ch.gender = app_identity_3,
          ch.race = app_identity_4) %>%
-  select(starts_with("ch"), id, pid3final)
+  select(starts_with("ch"), id, ideo7) %>%
+  filter(!is.na(ideo7))
 
 head(dt) # check
 
 
 # Run Rank-order logit =========================================================
 library(mlogit)
-
+library(effects)
 
 # Transform into mlogit format
 dt <- as.data.frame(dt)
@@ -41,11 +43,11 @@ head(mdat)
 mdat$ch        # logical: TRUE if id2 was ranked idx1-th, by unit id1
 mdat$id        # respondent id
 mdat$idx       # position id (1st, 2nd, 3rd, 4th = depressed)
-mdat$pid3final # explanatory variable
+mdat$ideo7     # explanatory variable
 
 
 # Estimating parameters
-m <- mlogit(ch ~ 1 | pid3final,  # Y ~ X_item | X_resp
+m <- mlogit(ch ~ 1 | ideo7,  # Y ~ X_item | X_resp
             mdat,                # Data
             reflevel = "party"   # Base category
             ) 
@@ -54,15 +56,34 @@ m <- mlogit(ch ~ 1 | pid3final,  # Y ~ X_item | X_resp
 # Raw result
 summary(m)
 
+# Interpret results ============================================================
+
 # Fitted values for starter: 1082*3 = 3246 choices
 fit <- fitted(m, type = "probabilities")
 head(fit)
 
+dt2 <- dt %>% 
+  slice(rep(1:n(), each = 3)) %>%
+  group_by(id) %>%
+  mutate(stage = c("First choice", 
+                   "Second choice", 
+                   "Third choice")) %>%
+  ungroup() 
+
+dt2 <- cbind(dt2, fit) %>%
+  data.frame()
 
 
+# Visualize fitted values over ideology
+p <-  ggplot(dt2, aes(x = ideo7, y = party)) +
+  geom_point(color = "maroon") +
+  facet_wrap( ~ stage, nrow = 1) +
+  theme_bw() +
+  ylab("Pr (Political Party)") +
+  xlab("Ideology")
 
-# Interpret results ============================================================
-
-
+p
+ggsave(here::here("fig", "regress.pdf"),
+       height = 4, width = 10)
 
 
