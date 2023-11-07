8daf78e7e6eb05fb714dd0a01ace393f256b7a67
Atsusaka <yatsusak@CougarNet.UH.EDU>
2023-11-07 15:56:45 -0600

    add MDPREF

--- R/06_applications_unfold.R
+++ R/06_applications_unfold.R
@@ -53,31 +53,48 @@ rank_sample <- rank_sample %>%
   dplyr::select(-.)
 
 
-
 # Run Unfolding (there is no way to incorporate weights..., so I improvised)
 library(smacof)
 
 set.seed(142)
-out <- unfolding(dt[,1:4], type = "ordinal")
-out_w <- unfolding(rank_sample, type = "ordinal")
+out <- unfolding(dt[,1:4], type = "ordinal", circle = "column")
+out_w <- unfolding(rank_sample, type = "ordinal", circle = "column")
+
+# Vector Model of Unfolding (Principal Component Analysis for Ranking Data)
+set.seed(142)
+pca <- vmu(dt[,1:4])
+pca_w <- vmu(rank_sample)
 
 
-pdf(here::here("fig", "weighting_unfolding.pdf"), width = 9.5, height = 5)
-par(mfrow = c(1,2))
+
+
+pdf(here::here("fig", "weighting_unfolding.pdf"), width = 9.5, height = 10)
+par(mfrow = c(2,2))
 plot(out, ylim = c(-1, 1), 
      main = "Raw Data",
      type = "p", pch = 16, col.columns = "darkred", cex = 1.5,
      label.conf.columns = list(label = TRUE, pos = 3, col = "darkred", cex = 1.2),
-     col.rows = 8, label.conf.rows = list(label = F, pos = 3, col = 8))
-abline(v = 0, lty = 2, col = "gray80")
-abline(h = 0, lty = 2, col = "gray80")
+     col.rows = 8, label.conf.rows = list(label = F, pos = 3, col =  alpha("gray80", 0.4)))
+abline(v = 0, lty = 2, col = alpha("gray80", 0.4))
+abline(h = 0, lty = 2, col = alpha("gray80", 0.4))
 
 plot(out_w, ylim = c(-1, 1), 
      main = "With Bias Correction",
      type = "p", pch = 16, col.columns = "darkcyan", cex = 1.5,
      label.conf.columns = list(label = TRUE, pos = 3, col = "darkcyan", cex = 1.2),
-     col.rows = 8, label.conf.rows = list(label = F, pos = 3, col = 8))
-abline(v = 0, lty = 2, col = "gray80")
-abline(h = 0, lty = 2, col = "gray80")
+     col.rows = 8, label.conf.rows = list(label = F, pos = 3, col =  alpha("gray80", 0.4)))
+abline(v = 0, lty = 2, col = alpha("gray80", 0.4))
+abline(h = 0, lty = 2, col = alpha("gray80", 0.4))
+
+
+plot(pca, col = c("darkred", alpha("gray80", 0.01)), 
+     cex = c(1, 0.7), xlim = c(-1.5, 1.5))
+plot(pca_w, col = c("darkcyan", alpha("gray80", 0.01)), 
+     cex = c(1, 0.7), xlim = c(-1.5, 1.5))
+
 
 dev.off()
+
+
+
+
