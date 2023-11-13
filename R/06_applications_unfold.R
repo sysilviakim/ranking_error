768149de9ae97f9f01896e93bb44d344c92c0218
Atsusaka <yatsusak@CougarNet.UH.EDU>
2023-11-13 10:38:12 -0600

    save the final unfolding graph

--- R/06_applications_unfold.R
+++ R/06_applications_unfold.R
@@ -12,6 +12,17 @@ main <- main %>%
   )
 
 # Reference set: (party, religion, gender, race)
+
+
+# Grab rankings and weights
+imp_w <- read_csv(here::here("data/tidy", "temp_weight.csv")) %>%
+  mutate(
+    app_identity = as.character(ranking),
+    bias_weight = weight
+  ) %>%
+  dplyr::select(app_identity, est.x.adj)
+
+
 # Downsize data
 dt <- main %>%
   mutate(id = 1:nrow(main)) %>%
@@ -22,18 +33,11 @@ dt <- main %>%
     race_ethnicity = app_identity_4
   ) %>%
   left_join(imp_w, by = "app_identity") %>%
+  rename(bias_weight = est.x.adj) %>%
   dplyr::select(
     party, religion, gender, race_ethnicity, id, ideo7, bias_weight
   ) %>%
   filter(!is.na(ideo7))
-
-# Grab rankings and weights
-imp_w <- read_csv(here::here("data/tidy", "temp_weight.csv")) %>%
-  mutate(
-    app_identity = as.character(ranking),
-    bias_weight = weight
-  ) %>%
-  dplyr::select(app_identity, est.x.adj)
 
 # "sample" from correct PMF
 set.seed(142)
@@ -65,6 +69,20 @@ out_w <- unfolding(rank_sample, type = "ordinal", circle = "column")
 set.seed(142)
 pca <- vmu(dt[, 1:4])
 pca_w <- vmu(rank_sample)
+
+
+pdf(here::here("fig", "weighting_unfolding.pdf"), width = 6, height = 6)
+par(mfrow = c(1, 1))
+plot(pca_w,
+     col = c("darkcyan", alpha("gray80", 0.1)),
+     cex = c(1, 0.7), xlim = c(-1.5, 1.5),
+     xlab = "Dimension 1",
+     ylab = "Dimension 2"
+)
+abline(v = 0, lty = 2, col = alpha("black", 0.4))
+abline(h = 0, lty = 2, col = alpha("black", 0.4))
+dev.off()
+
 
 
 
