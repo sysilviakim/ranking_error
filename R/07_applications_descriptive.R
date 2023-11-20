791465e2a79c72905c29ce1e85857725a547c1f0
Atsusaka <yatsusak@CougarNet.UH.EDU>
2023-11-20 15:22:06 -0600

    update PMF with ordering

--- R/07_applications_descriptive.R
+++ R/07_applications_descriptive.R
@@ -1,6 +1,9 @@
 source(here::here("R", "utilities.R"))
 load(here("data", "tidy", "df_list.Rda"))
 
+library(questionr) # for wtd.table()
+library(RColorBrewer)  
+
 # Grab rankings and weights
 imp_w <- read_csv(here::here("data/tidy", "temp_weight.csv")) %>%
   mutate(
@@ -33,7 +36,7 @@ dt <- main %>%
   left_join(imp_w, by = "app_identity") %>%
   select(
     party, religion, gender, race_ethnicity, id,
-    ideo7, pid7, race, bias_weight
+    ideo7, pid7, race, app_identity, bias_weight
   ) %>%
   filter(
     !is.na(ideo7),
@@ -42,7 +45,72 @@ dt <- main %>%
   )
 
 
-# Visualize descriptive statistics
+perm <- combinat::permn(x = c("Party","Religion","Gender","Race")) 
+
+
+pmf <- wtd.table(x = dt$app_identity,
+                 weights = dt$bias_weight/dim(dt)[1]) %>%
+  data.frame() %>%
+  mutate(party = substr(Var1, 1, 1),
+         religion = substr(Var1, 2, 2),
+         gender = substr(Var1, 3, 3),
+         race = substr(Var1, 4, 4),
+         type = case_when(party == 1 ~ "A",
+                          religion == 1 ~ "B",
+                          gender == 1 ~ "C",
+                          race == 1 ~ "D"),
+         ordering = case_when(Var1 == 1234 ~ "Party-Religion-Gender-Race",
+                              Var1 == 1243 ~ "Party-Religion-Race-Gender",
+                              Var1 == 1324 ~ "Party-Gender-Religion-Race",
+                              Var1 == 1342 ~ "Party-Race-Religion-Gender",
+                              Var1 == 1423 ~ "Party-Gender-Race-Religion",
+                              Var1 == 1432 ~ "Party-Race-Gender-Religion",
+                              Var1 == 2134 ~ "Religion-Party-Gender-Race",
+                              Var1 == 2143 ~ "Religion-Party-Race-Gender",
+                              Var1 == 2314 ~ "Gender-Party-Religion-Race",
+                              Var1 == 2341 ~ "Race-Party-Religion-Gender",
+                              Var1 == 2413 ~ "Gender-Party-Race-Religion",
+                              Var1 == 2431 ~ "Race-Party-Gender-Religion",
+                              Var1 == 3124 ~ "Religion-Gender-Party-Race",
+                              Var1 == 3142 ~ "Religion-Race-Party-Gender",
+                              Var1 == 3214 ~ "Gender-Religion-Party-Race",
+                              Var1 == 3241 ~ "Race-Religion-Party-Gender",
+                              Var1 == 3412 ~ "Gender-Race-Party-Religion",
+                              Var1 == 3421 ~ "Race-Gender-Party-Religion",
+                              Var1 == 4123 ~ "Religion-Gender-Race-Party",
+                              Var1 == 4132 ~ "Religion-Race-Gender-Party",
+                              Var1 == 4213 ~ "Gender-Religion-Race-Party",
+                              Var1 == 4231 ~ "Race-Religion-Gender-Party",
+                              Var1 == 4312 ~ "Gender-Race-Religion-Party",
+                              Var1 == 4321 ~ "Race-Gender-Religion-Party")) %>%
+  arrange(type, Var1)
+
+
+
+
+p <- ggplot(pmf, aes(x = ordering, y = Freq, fill = type)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = brewer.pal(n = 4, name = 'Set2')) +
+  geom_vline(xintercept = 1/24, lty=2, color = "dimgray") +
+  ylab("Proportion of Unique Ranking Profiles") +
+  xlab("") +
+  theme_classic() +
+  theme(legend.position = "none") +
+  coord_flip()
+
+p
+
+ggsave(here::here("fig", "weight-PMF.pdf"),
+       width = 5, height = 8)
+
+
+# Visualize full PMF============================================================
+
+
+
+
+
+# Visualize descriptive statistics =============================================
 
 target <- "party"
 others <- c("religion", "gender", "race_ethnicity")
