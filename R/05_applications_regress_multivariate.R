19ff27fa2614a28aa6903de7d6870ea7a943838f
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2023-12-23 09:01:02 +0900

    experimenting with dgesv error

--- R/05_applications_regress_multivariate.R
+++ R/05_applications_regress_multivariate.R
@@ -4,7 +4,6 @@ load(here("data", "tidy", "df_list.Rda"))
 # devtools::install_github("sysilviakim/ranking", force = T)
 library(ranking)
 
-
 # Grab rankings and weights
 imp_w <- read_csv(here::here("data/tidy", "temp_weight.csv")) %>%
   mutate(
@@ -22,12 +21,9 @@ main <- df_list$main
 # Data processing ==============================================================
 # Fix some weird situation ---> haven::labelled to numeric
 main <- main %>%
-  mutate(
-    across(where(is.labelled), ~ as.numeric(as.character(.)))
-  )
+  mutate(across(where(is.labelled), ~ as.numeric(as.character(.))))
 
 # Reference set: (party, religion, gender, race)
-
 # Downsize data
 dt <- main %>%
   mutate(
@@ -48,15 +44,22 @@ dt <- main %>%
     age, partisan, region, gender3,
     bias_weight
   ) %>%
-  drop_na()
-
+  drop_na() %>%
+  ## Aggregate Native American, Other, and Middle Eastern into a single category
+  ## for race (otherwise, singularity issue)
+  mutate(
+    race6 = case_when(
+      as.numeric(race) %in% c(5, 7, 8) ~ 5,
+      TRUE ~ as.numeric(race)
+    ),
+    race6 = as.factor(race6)
+  )
 
 head(dt) # check
 hist(dt$bias_weight, border = "white") # good, no extremely small weight
 abline(v = 1, lty = 2, lwd = 2, col = "darkred")
 
-
-# Run Rank-order logit =========================================================
+# Create indexed data ==========================================================
 library(mlogit)
 
 # Transform into mlogit format
@@ -74,8 +77,13 @@ mdat <- dfidx::dfidx(
 head(mdat)
 
 bootstrapped_mdat <- bootstrap_list(
-  dt, dfidx = TRUE, shape = "wide", n = 1000, seed = 123,
+  dt,
+  dfidx = TRUE, shape = "wide", n = 1000, seed = 123,
   choice = "ch", varying = 1:4, ranked = TRUE
+)
+save(
+  bootstrapped_mdat,
+  file = here::here("output", "bootstrapped_mdat_1000.Rda")
 )
 
 mdat$ch # logical: TRUE if id2 was ranked idx1-th, by unit id1
@@ -83,7 +91,7 @@ mdat$id # respondent id
 mdat$idx # position id (1st, 2nd, 3rd, 4th = depressed)
 mdat$ideo7 # explanatory variable
 
-
+# Run Rank-order logit model ===================================================
 # Estimating parameters (no weight)
 m <- mlogit(
   ch ~ 1 | ideo7 + pid7 + educ + race + age + partisan + region + gender3,
@@ -106,6 +114,48 @@ summary(m)
 summary(m2)
 
 # save mlogit object for checking
-saveRDS(m, file = here::here("output", "full_raw.RData"))
+save(m, file = here::here("output", "full_raw.Rda"))
 # save mlogit object for checking
-saveRDS(m2, file = here::here("output", "full_correct.RData"))
+save(m2, file = here::here("output", "full_correct.Rda"))
+
+# Bootstrapped version =========================================================
+# Estimating parameters (no weight)
+m_noweight_boot <- bootstrapped_mdat %>%
+  imap(
+    ~ tryCatch(
+      {
+        out <- mlogit(
+          ## https://cran.r-project.org/web/packages/mlogit/vignettes/c2.formula.data.html
+          ## ch ~ 1 | ideo7 + race + gender3, ---> error, worked fine if without race
+          ## ch ~ 1 | ideo7 + age + educ + partisan + gender3, ---> this works
+          ## If you exclude race, it works fine. Some bad combination?
+          ch ~ 1 | 
+            ideo7 + pid7 + educ + age + partisan + region + gender3,
+          .x,
+          reflevel = "gender"
+        )
+        return(out)
+      },
+      error = function(e) {
+        ## Error: Lapack routine dgesv: system is exactly singular: U[31,31] = 0
+        ## 1, 5, 12, 18, 19, 20, 23, 26, 28, 33, ...
+        message("Error: ", e$message)
+        message("The error occurred at the ", .y, "th iteration")
+        return(NULL)
+      }
+    )
+  )
+save(m_noweight_boot, file = here::here("output", "m_noweight_boot_norace.Rda"))
+
+# Estimating parameters (with weight)
+m_weighted_boot <- bootstrapped_mdat %>%
+  map(
+    ~ mlogit(
+      ## Excluded race
+      ch ~ 1 | ideo7 + pid7 + educ + age + partisan + region + gender3,
+      .x,
+      reflevel = "gender",
+      weight = bias_weight
+    )
+  )
+save(m_weighted_boot, file = here::here("output", "m_weighted_boot_norace.Rda"))
