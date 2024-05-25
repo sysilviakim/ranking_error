a5255db9e07e18d362ba2893fe3cb791f8b6cb54
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-05-25 21:10:37 +0900

    allow J = NULL + styler::styler

--- R/imprr_direct.R
+++ R/imprr_direct.R
@@ -25,25 +25,25 @@
 #' @param anchor_q Column name for the paired anchor question.
 #' @param anc_correct Indicator for passing the anchor question.
 #' @param anc_correct_pattern The correct pattern to pass the anchor question
-#' filter, given the reference set. Defaults to NULL. 
-#' If NULL, it will taken on a J-length string with a natural progression of 
+#' filter, given the reference set. Defaults to NULL.
+#' If NULL, it will taken on a J-length string with a natural progression of
 #' numbers, such as "123", "1234", "1234567", and so on.
 #' @param main_labels Labels for ranking options in the main question.
 #' @param n_bootstrap Number of bootstraps. Defaults to 1,000.
 #' @param asymptotics Whether the computations use asymptotics or the given
 #' data. Defaults to FALSE.
 #' @param seed Seed for \text{set.seed} for reproducibility.
-#' 
+#'
 
 # # # # For coding
 # source(here::here("R", "utilities.R"))
 # load(here("data", "tidy", "df_list.Rda"))
-# 
+#
 # data <- df_list$main %>%
 #   select(app_identity_1, app_identity_2, app_identity_3, app_identity_4,
 #          anc_identity_1, anc_identity_2, anc_identity_3, anc_identity_4,
 #          anc_correct_identity)
-# 
+#
 # main_q <- "app_identity"
 # anchor_q <- "anc_identity"
 # anc_correct <- "anc_correct_identity"
@@ -52,18 +52,19 @@
 # seed = 123456
 
 imprr_direct <- function(data,
-                         J,
+                         J = NULL,
                          main_q,
                          anchor_q,
                          anc_correct,
                          anc_correct_pattern = NULL,
                          n_bootstrap = 200,
-#                        asymptotics = TRUE,
                          seed = 123456) {
   # Setup ======================================================================
   N <- nrow(data)
-#  J <- nchar(data[[main_q]][[1]])
-  
+  if (is.null(J)) {
+    J <- nchar(data[[main_q]][[1]])
+  }
+
   # # Check the validity of the input arguments ==================================
   # if (!(main_q %in% names(data))) {
   #   stop("The main question is not a valid column name in the given dataset.")
@@ -96,12 +97,12 @@ imprr_direct <- function(data,
   # if (nchar(anc_correct_pattern) != J) {
   #   stop("Length of anc_correct_pattern does not match J.")
   # }
-  
+
   ## List for bootstrapped results
   list_weights <- list_qoi <- list_prop <- vector("list", length = n_bootstrap)
-  
 
-  
+
+
   # Boostrapping ===============================================================
   ## We bootstrap to account for uncertainty from the estimation of Pr(random)
   ## Sample with replacement
@@ -109,167 +110,179 @@ imprr_direct <- function(data,
   ### i: over n_bootstrap
   ### j: over J items
   ### k: over K estimates
-  
+
   set.seed(seed)
   for (i in 1:n_bootstrap) {
     ## Sample indices
     index <- sample(1:nrow(data), size = nrow(data), replace = TRUE)
-    
+
     ## This is the bootstrapped data
     boostrap_dat <- data[index, ]
-    
+
     ## Anchor ranking only
     loc_anc <- boostrap_dat %>%
       select(matches(anchor_q)) %>%
       select(matches("_[[:digit:]]$"))
-    
+
     ## Main ranking only (Silvia, I edited here slightly)
     loc_app <- boostrap_dat %>%
       select(matches(main_q)) %>%
       select(matches("_[[:digit:]]$"))
-    
-    
+
+
     # Step 1: Get the proportion of random answers
     ## This requires anchor questions and item order randomization
     p_non_random <- (mean(boostrap_dat[[anc_correct]]) - 1 / factorial(J)) /
       (1 - 1 / factorial(J))
 
     # Step 2: Get the naive estimates of simple quantities
-    
+
     item_names <- colnames(loc_app)
     J_1 <- J - 1
-      
+
     all_qoi_list <- list()
 
-    for(j in 1:J){
-    
-    # Specify each item as the target item in return  
-    target_item <- item_names[j]
-    other_items <- item_names[-j]      
-        
-    # Step 2.1: Locally code a few quantities
-    ## Marginal rank
-    Y_rank_target <- boostrap_dat[target_item] %>% pull() 
+    for (j in 1:J) {
+      # Specify each item as the target item in return
+      target_item <- item_names[j]
+      other_items <- item_names[-j]
 
-    ## Pairwise ranking indicator    
-    Y_pairwise <- list()                
-    for(k in 1:J_1){
-      compar <- boostrap_dat[other_items[k]] %>% pull() # Comparison item
-      Y_pairwise[[k]] <- ifelse(Y_rank_target < compar, 1, 0)
-    }
-    
-    # Top-k ranking indicator
-    Y_top <- list()
-    for(k in 1:J_1){
-      Y_top[[k]] <- ifelse(Y_rank_target <= k, 1, 0)
-    }
-    
-    # Marginal ranking indicator
-    Y_marginal <- list()
-    tgt <- boostrap_dat[target_item] %>% pull()  
-    for(k in 1:J){
-      Y_marginal[[k]] <- ifelse(tgt == k, 1, 0)  
-    }
+      # Step 2.1: Locally code a few quantities
+      ## Marginal rank
+      Y_rank_target <- boostrap_dat[target_item] %>% pull()
 
-    
-    # Step 2.2: Get raw estimates of
-    ## Average ranks
-    m_rank_target <- lm_robust(Y_rank_target ~ 1) %>% tidy()
-    
-    ## Pairwise ranking probabilities
-    m_pairwise <- list()
-    for(k in 1:J_1){
-      m_pairwise[[k]] <- lm_robust(Y_pairwise[[k]] ~ 1) %>% tidy()
-    }
+      ## Pairwise ranking indicator
+      Y_pairwise <- list()
+      for (k in 1:J_1) {
+        compar <- boostrap_dat[other_items[k]] %>% pull() # Comparison item
+        Y_pairwise[[k]] <- ifelse(Y_rank_target < compar, 1, 0)
+      }
 
-    ## Top-k ranking probabilities    
-    m_top <- list()
-    for(k in 1:J_1){
-      m_top[[k]] <- lm_robust(Y_top[[k]] ~ 1) %>% tidy()
-    }
-    
-    ## Marginal ranking probabilities
-    m_marginal <- list()
-    for(k in 1:J){
-      m_marginal[[k]] <- lm_robust(Y_marginal[[k]] ~ 1) %>% tidy()
-    }  
+      # Top-k ranking indicator
+      Y_top <- list()
+      for (k in 1:J_1) {
+        Y_top[[k]] <- ifelse(Y_rank_target <= k, 1, 0)
+      }
 
-    
-    # Step 3: Get the QOI based on random responses   
-    ## g(random)---QOI based on uniform distribution    
-    gg_averagerank <- m_rank_target %>%
-      select(estimate) %>%
-      mutate(outcome = paste0('Avg:', ' ', target_item),
-             qoi = "average rank",
-             g_U = (1+J)/2)
-    
-    gg_pairwise <- do.call(rbind.data.frame, m_pairwise) %>%
-      select(estimate) %>%      
-      mutate(outcome = paste0('v.', ' ', other_items),
-             qoi = "pairwise ranking",
-             g_U = 0.5)
-    
-    gg_topk <- do.call(rbind.data.frame, m_top) %>%
-      select(estimate) %>%  
-      mutate(outcome = paste0('Top-', '', 1:J_1),
-             qoi = "top-k ranking",
-             g_U = 1/J)
-    
-    gg_marginal <- do.call(rbind.data.frame, m_marginal) %>%
-      select(estimate) %>%  
-      mutate(outcome = paste0('Ranked', ' ', 1:J),
-             qoi = "marginal ranking",
-             g_U = 1/J)
-    
-    
-    # Step 4: Directly apply bias-correction (Equation 6)
-    ## bc_estimate: bias-corrected estimate
-    all_qoi <- rbind(gg_averagerank,
-                     gg_pairwise,
-                     gg_topk,
-                     gg_marginal) %>%
-      mutate(bc_estimate = (estimate - (g_U * (1 - p_non_random))) / p_non_random,
-             item = target_item)
-    
-    ## Save the estimates for all quantities
-    all_qoi_list[[j]] <- all_qoi
-    
+      # Marginal ranking indicator
+      Y_marginal <- list()
+      tgt <- boostrap_dat[target_item] %>% pull()
+      for (k in 1:J) {
+        Y_marginal[[k]] <- ifelse(tgt == k, 1, 0)
+      }
+
+
+      # Step 2.2: Get raw estimates of
+      ## Average ranks
+      m_rank_target <- lm_robust(Y_rank_target ~ 1) %>% tidy()
+
+      ## Pairwise ranking probabilities
+      m_pairwise <- list()
+      for (k in 1:J_1) {
+        m_pairwise[[k]] <- lm_robust(Y_pairwise[[k]] ~ 1) %>% tidy()
+      }
+
+      ## Top-k ranking probabilities
+      m_top <- list()
+      for (k in 1:J_1) {
+        m_top[[k]] <- lm_robust(Y_top[[k]] ~ 1) %>% tidy()
+      }
+
+      ## Marginal ranking probabilities
+      m_marginal <- list()
+      for (k in 1:J) {
+        m_marginal[[k]] <- lm_robust(Y_marginal[[k]] ~ 1) %>% tidy()
+      }
+
+
+      # Step 3: Get the QOI based on random responses
+      ## g(random)---QOI based on uniform distribution
+      gg_averagerank <- m_rank_target %>%
+        select(estimate) %>%
+        mutate(
+          outcome = paste0("Avg:", " ", target_item),
+          qoi = "average rank",
+          g_U = (1 + J) / 2
+        )
+
+      gg_pairwise <- do.call(rbind.data.frame, m_pairwise) %>%
+        select(estimate) %>%
+        mutate(
+          outcome = paste0("v.", " ", other_items),
+          qoi = "pairwise ranking",
+          g_U = 0.5
+        )
+
+      gg_topk <- do.call(rbind.data.frame, m_top) %>%
+        select(estimate) %>%
+        mutate(
+          outcome = paste0("Top-", "", 1:J_1),
+          qoi = "top-k ranking",
+          g_U = 1 / J
+        )
+
+      gg_marginal <- do.call(rbind.data.frame, m_marginal) %>%
+        select(estimate) %>%
+        mutate(
+          outcome = paste0("Ranked", " ", 1:J),
+          qoi = "marginal ranking",
+          g_U = 1 / J
+        )
+
+
+      # Step 4: Directly apply bias-correction (Equation 6)
+      ## bc_estimate: bias-corrected estimate
+      all_qoi <- rbind(
+        gg_averagerank,
+        gg_pairwise,
+        gg_topk,
+        gg_marginal
+      ) %>%
+        mutate(
+          bc_estimate = (estimate - (g_U * (1 - p_non_random))) / p_non_random,
+          item = target_item
+        )
+
+      ## Save the estimates for all quantities
+      all_qoi_list[[j]] <- all_qoi
     } # End of j loop
 
-    
-      
-  all_qoi_df <- do.call(rbind.data.frame, all_qoi_list)
-  
-  list_prop[[i]] <- 1 - p_non_random # Estimated prop of random responses
-  list_qoi[[i]] <- all_qoi_df        # Bias-corrected estimates of several QOIs  
-        
-    
+
+
+    all_qoi_df <- do.call(rbind.data.frame, all_qoi_list)
+
+    list_prop[[i]] <- 1 - p_non_random # Estimated prop of random responses
+    list_qoi[[i]] <- all_qoi_df # Bias-corrected estimates of several QOIs
   } # End of i loop
   message("Bootstrapping finished.")
 
 
-  
+
   # Summarize results
   ## Compute the mean and 95% CI based on bootstrapping
   ## Return all weights for IPW
-  
+
   df_random <- do.call(rbind.data.frame, list_prop)
   colnames(df_random) <- "est.p.random"
   df_random_summary <- df_random %>%
-    summarize(mean = mean(est.p.random),
-              lower = quantile(est.p.random, 0.025),
-              upper = quantile(est.p.random, 0.975))
-  
+    summarize(
+      mean = mean(est.p.random),
+      lower = quantile(est.p.random, 0.025),
+      upper = quantile(est.p.random, 0.975)
+    )
+
   df_qoi_summary <- do.call(rbind.data.frame, list_qoi) %>%
     group_by(item, qoi, outcome) %>%
     arrange(bc_estimate) %>%
-    summarize(mean = mean(bc_estimate),
-              lower = quantile(bc_estimate, 0.025),
-              upper = quantile(bc_estimate, 0.975))
+    summarize(
+      mean = mean(bc_estimate),
+      lower = quantile(bc_estimate, 0.025),
+      upper = quantile(bc_estimate, 0.975)
+    )
 
-  
-  return(list(est_p_random = df_random_summary,
-              qoi = df_qoi_summary)
-  )
-  
+
+  return(list(
+    est_p_random = df_random_summary,
+    qoi = df_qoi_summary
+  ))
 }
--- R/imprr_weights.R
+++ R/imprr_weights.R
@@ -25,24 +25,24 @@
 #' @param anchor_q Column name for the paired anchor question.
 #' @param anc_correct Indicator for passing the anchor question.
 #' @param anc_correct_pattern The correct pattern to pass the anchor question
-#' filter, given the reference set. Defaults to NULL. 
-#' If NULL, it will taken on a J-length string with a natural progression of 
+#' filter, given the reference set. Defaults to NULL.
+#' If NULL, it will taken on a J-length string with a natural progression of
 #' numbers, such as "123", "1234", "1234567", and so on.
 #' @param main_labels Labels for ranking options in the main question.
 #' @param n_bootstrap Number of bootstraps. Defaults to 1,000.
 #' @param asymptotics Whether the computations use asymptotics or the given
 #' data. Defaults to FALSE.
 #' @param seed Seed for \text{set.seed} for reproducibility.
-#' 
+#'
 
 # source(here::here("R", "utilities.R"))
 # load(here("data", "tidy", "df_list.Rda"))
-# 
+#
 # data <- df_list$main %>%
 #   select(app_identity_1, app_identity_2, app_identity_3, app_identity_4,
 #          anc_identity_1, anc_identity_2, anc_identity_3, anc_identity_4,
 #          anc_correct_identity)
-# 
+#
 # main_q <- "app_identity"
 # anchor_q <- "anc_identity"
 # anc_correct <- "anc_correct_identity"
@@ -51,18 +51,19 @@
 # seed = 123456
 
 imprr_weights <- function(data,
-                          J,
+                          J = NULL,
                           main_q,
                           anchor_q,
                           anc_correct,
                           anc_correct_pattern = NULL,
                           n_bootstrap = 200,
-#                         asymptotics = TRUE,
                           seed = 123456) {
   # Setup ======================================================================
   N <- nrow(data)
-#  J <- nchar(data[[main_q]][[1]])
-  
+  if (is.null(J)) {
+    J <- nchar(data[[main_q]][[1]])
+  }
+
   # # Check the validity of the input arguments ==================================
   # if (!(main_q %in% names(data))) {
   #   stop("The main question is not a valid column name in the given dataset.")
@@ -95,13 +96,13 @@ imprr_weights <- function(data,
   # if (nchar(anc_correct_pattern) != J) {
   #   stop("Length of anc_correct_pattern does not match J.")
   # }
-  
+
 
   ## Anchor ranking only
   glo_anc <- data %>%
     select(matches(anchor_q)) %>%
     select(matches("_[[:digit:]]$"))
-  
+
   ## Main ranking only (Silvia, I edited here slightly)
   glo_app <- data %>%
     select(matches(main_q)) %>%
@@ -110,25 +111,26 @@ imprr_weights <- function(data,
   # Step 1: Get the proportion of random answers
   p_non_random <- (mean(data[[anc_correct]]) - 1 / factorial(J)) /
     (1 - 1 / factorial(J))
-  
+
   # Step 2: Get the uniform distribution
   U <- rep(1 / factorial(J), factorial(J))
 
-  # Step 3: Get the observed PMF based on raw data  
+  # Step 3: Get the observed PMF based on raw data
   ## Get raw counts of ranking profiles
   D_PMF_0 <- glo_app %>%
     unite(ranking, sep = "") %>%
     group_by(ranking) %>%
     count()
-  
+
   ## Create sample space to merge
   perm_j <- permn(1:J)
   perm_j <- do.call(rbind.data.frame, perm_j)
   colnames(perm_j) <- c(paste0("position_", 1:J))
-  perm_j <- perm_j %>% unite(col = "ranking", sep = "") %>%
+  perm_j <- perm_j %>%
+    unite(col = "ranking", sep = "") %>%
     arrange(ranking)
 
-  ## We need this because some rankings may not appear in the data  
+  ## We need this because some rankings may not appear in the data
   PMF_raw <- perm_j %>%
     left_join(D_PMF_0, by = "ranking") %>%
     mutate(
@@ -137,41 +139,47 @@ imprr_weights <- function(data,
       prop = ifelse(is.na(prop), 0, prop)
     ) %>%
     arrange(ranking)
-  
-  
+
+
   # Step 4: Get the bias-corrected PMF
   ## Apply Equation A.11
   imp_PMF_0 <- (PMF_raw$prop - (U * (1 - p_non_random))) / p_non_random
-  
+
   ## Recombine with ranking ID
   imp_PMF_1 <- perm_j %>%
-    mutate(n = imp_PMF_0) 
-  
-  # Step 5: Re-normalize the PMF     
-  ## The previous step may produce outside-the-bound values (negative proportions) 
-  imp_PMF <- imp_PMF_1 %>%
-    mutate(n_adj = ifelse(n < 0, 0, n),
-           n_renormalized = n_adj / sum(n_adj)) %>%
-    rename(prop = n,
-           prop_adj = n_adj,
-           prop_renormalized = n_renormalized) %>%
-    arrange(ranking)
-  
-  # Step 6: Get the bias-correction weight vector
-    df_w <- perm_j %>%
-    mutate(w = imp_PMF$prop_renormalized / PMF_raw$prop, # Inverse probability weight
-           w = ifelse(w == Inf, 0, w),
-           w = ifelse(is.na(w), 0, w)) %>%  # NA arise from 0/0
-      arrange(ranking)
+    mutate(n = imp_PMF_0)
 
-    
-    
+  # Step 5: Re-normalize the PMF
+  ## The previous step may produce outside-the-bound values (negative proportions)
+  imp_PMF <- imp_PMF_1 %>%
+    mutate(
+      n_adj = ifelse(n < 0, 0, n),
+      n_renormalized = n_adj / sum(n_adj)
+    ) %>%
+    rename(
+      prop = n,
+      prop_adj = n_adj,
+      prop_renormalized = n_renormalized
+    ) %>%
+    arrange(ranking)
+
+  # Step 6: Get the bias-correction weight vector
+  df_w <- perm_j %>%
+    mutate(
+      w = imp_PMF$prop_renormalized / PMF_raw$prop, # Inverse probability weight
+      w = ifelse(w == Inf, 0, w),
+      w = ifelse(is.na(w), 0, w)
+    ) %>% # NA arise from 0/0
+    arrange(ranking)
+
+
+
   # Summarize results
-    
-  return(list(est_p_random = 1 - p_non_random,
-              obs_pmf = PMF_raw,
-              corrected_pmf = imp_PMF,
-              weights = df_w)
-  )
-  
+
+  return(list(
+    est_p_random = 1 - p_non_random,
+    obs_pmf = PMF_raw,
+    corrected_pmf = imp_PMF,
+    weights = df_w
+  ))
 }
