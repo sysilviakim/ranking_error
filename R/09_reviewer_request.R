a39e3fbca03cffff2ca6feae42229b8d84bc1a0f
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-06-29 15:10:06 +0900

    table 2 renew

--- R/09_reviewer_request.R
+++ R/09_reviewer_request.R
@@ -111,3 +111,33 @@ ggsave(
   here("fig", "predict_random_identity_coefplot.pdf"),
   width = 9, height = 5
 )
+
+# Distribution of responses to the anchor question =============================
+tab <- table(main$anc_identity)
+## Turn to tibble after name repairing
+tab <- as_tibble(tab, .name_repair = "unique") %>%
+  rename(Permutation = `...1`, Frequency = n) %>%
+  mutate(`Percent (%)` = round(Frequency / sum(Frequency) * 100, 1))
+
+## Export this into two-column table
+tab <- cbind(
+  tab[1:12, ],
+  bind_rows(
+    tab[13:nrow(tab), ],
+    tibble(Permutation = NA, Frequency = NA, `Percent (%)` = NA)
+  )
+)
+
+xtable(
+  tab,
+  caption = "Distribution of Responses to the Anchor Question",
+  label = "tab:anchor_detail",
+  align = "llrrlrr"
+) %>%
+  print(
+    type = "latex",
+    file = here("tab", "anchor_detail.tex"),
+    include.rownames = FALSE,
+    floating = FALSE,
+    booktabs = TRUE
+  )
