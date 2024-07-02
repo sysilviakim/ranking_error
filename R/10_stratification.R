47e55e958191df374672cdd2c38d3d437b9132bf
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2024-07-02 15:00:24 +0900

    apply styler::styler

--- R/10_stratification.R
+++ R/10_stratification.R
@@ -7,9 +7,11 @@ main <- df_list$main %>%
 
 ## Reference set: (party, religion, gender, race)
 identity_data <- main %>%
-  mutate(pid_recode = case_when(pid3 == 1 ~ "Democrat",
-                                pid3 == 2 ~ "Republican",
-                                TRUE ~ "Others")) %>%
+  mutate(pid_recode = case_when(
+    pid3 == 1 ~ "Democrat",
+    pid3 == 2 ~ "Republican",
+    TRUE ~ "Others"
+  )) %>%
   select(
     app_identity_1, app_identity_2, app_identity_3, app_identity_4,
     anc_identity_1, anc_identity_2, anc_identity_3, anc_identity_4,
@@ -18,84 +20,97 @@ identity_data <- main %>%
     pid_recode
   )
 
-
 # Stratification by bootstrap  =================================================
 
 set.seed(NULL)
 seed_strat <- 1245
 n_bootstrap <- 200
-out_stratification <- data.frame(mean = as.numeric(),
-                                 item = as.character())
+out_stratification <- data.frame(
+  mean = as.numeric(),
+  item = as.character()
+)
 
 # set.seed(seed_strat) Using random seed here fixes all bootstrap estimates
 for (b in 1:n_bootstrap) {
-## Sample indices
-index <- sample(1:nrow(identity_data), size = nrow(identity_data), replace = TRUE)
-  
-## This is the bootstrapped data
-boostrap_dat <- identity_data [index, ]
+  ## Sample indices
+  index <- 
+    sample(1:nrow(identity_data), size = nrow(identity_data), replace = TRUE)
 
-## Estimated proportions of strata
-p_X <- prop.table(table(boostrap_dat$pid_recode))
-  
-## Stratify by partisanship
-data_dem <- boostrap_dat %>% filter(pid_recode == "Democrat")
-data_rep <- boostrap_dat %>% filter(pid_recode == "Republican")
-data_oth <- boostrap_dat %>% filter(pid_recode == "Others")
+  ## This is the bootstrapped data
+  boostrap_dat <- identity_data[index, ]
 
-# Apply bias correction ========================================================
-## Democrat --------------------------------------------------------------------
-direct_dem <- imprr_direct(
-  data = data_dem,
-  J = 4,
-  main_q = "app_identity",
-  anc_correct = "anc_correct_identity",
-  weight = data_dem$weight,
-  n_bootstrap = 1
-)
+  ## Estimated proportions of strata
+  p_X <- prop.table(table(boostrap_dat$pid_recode))
 
-## Republican ------------------------------------------------------------------
-direct_rep <- imprr_direct(
-  data = data_rep,
-  J = 4,
-  main_q = "app_identity",
-  anc_correct = "anc_correct_identity",
-  weight = data_rep$weight,
-  n_bootstrap = 1
-)
+  ## Stratify by partisanship
+  data_dem <- boostrap_dat %>% filter(pid_recode == "Democrat")
+  data_rep <- boostrap_dat %>% filter(pid_recode == "Republican")
+  data_oth <- boostrap_dat %>% filter(pid_recode == "Others")
 
-## Others ----------------------------------------------------------------------
-direct_oth <- imprr_direct(
-  data = data_oth,
-  J = 4,
-  main_q = "app_identity",
-  anc_correct = "anc_correct_identity",
-  weight = data_oth$weight,
-  n_bootstrap = 1
-)
+  # Apply bias correction ======================================================
+  ## Democrat ------------------------------------------------------------------
+  direct_dem <- imprr_direct(
+    data = data_dem,
+    J = 4,
+    main_q = "app_identity",
+    anc_correct = "anc_correct_identity",
+    weight = data_dem$weight,
+    n_bootstrap = 1
+  )
 
-# Stratification estimate ======================================================
+  ## Republican ----------------------------------------------------------------
+  direct_rep <- imprr_direct(
+    data = data_rep,
+    J = 4,
+    main_q = "app_identity",
+    anc_correct = "anc_correct_identity",
+    weight = data_rep$weight,
+    n_bootstrap = 1
+  )
 
-## Stratification estimate
-est_dem <- direct_dem$qoi %>% filter(qoi == "average rank") %>% ungroup() %>% select(item, mean)
-est_rep <- direct_rep$qoi %>% filter(qoi == "average rank") %>% ungroup() %>% select(item, mean)
-est_oth <- direct_oth$qoi %>% filter(qoi == "average rank") %>% ungroup() %>% select(item, mean)
+  ## Others --------------------------------------------------------------------
+  direct_oth <- imprr_direct(
+    data = data_oth,
+    J = 4,
+    main_q = "app_identity",
+    anc_correct = "anc_correct_identity",
+    weight = data_oth$weight,
+    n_bootstrap = 1
+  )
 
-strat <- 
-est_dem[1:4, 2] * p_X[1] + 
-  est_rep[1:4, 2] * p_X[2] + 
-  est_oth[1:4, 2] * p_X[3] 
+  # Stratification estimate ====================================================
 
-strat$item <- c("party", "religion", "gender", "race_ethnicity")
+  ## Stratification estimate
+  est_dem <- direct_dem$qoi %>%
+    filter(qoi == "average rank") %>%
+    ungroup() %>%
+    select(item, mean)
+  est_rep <- direct_rep$qoi %>%
+    filter(qoi == "average rank") %>%
+    ungroup() %>%
+    select(item, mean)
+  est_oth <- direct_oth$qoi %>%
+    filter(qoi == "average rank") %>%
+    ungroup() %>%
+    select(item, mean)
 
-out_stratification <- rbind(out_stratification, strat)
+  strat <-
+    est_dem[1:4, 2] * p_X[1] +
+    est_rep[1:4, 2] * p_X[2] +
+    est_oth[1:4, 2] * p_X[3]
+
+  strat$item <- c("party", "religion", "gender", "race_ethnicity")
+
+  out_stratification <- rbind(out_stratification, strat)
 }
 
 avg_gg_comb <- out_stratification %>%
-               group_by(item) %>%
-               summarize(estimate = mean(mean),
-               conf.low = quantile(mean, prob = 0.025),
-               conf.high = quantile(mean, prob = 0.975))
+  group_by(item) %>%
+  summarize(
+    estimate = mean(mean),
+    conf.low = quantile(mean, prob = 0.025),
+    conf.high = quantile(mean, prob = 0.975)
+  )
 
 # Visualization ================================================================
 
@@ -124,8 +139,8 @@ p <- avg_gg_comb %>%
     position = position_dodge(width = width_par), size = 1.5
   ) +
   geom_errorbar(aes(xmin = conf.low, xmax = conf.high),
-                width = 0,
-                position = position_dodge(width_par), size = 0.6
+    width = 0,
+    position = position_dodge(width_par), size = 0.6
   ) +
   scale_color_manual(
     values = c(
